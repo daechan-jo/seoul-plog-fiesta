8f974245f73eeb2d484ae67d5200df96c452a2fe
[AI8/1반] 지영은 <utael706@gmail.com>
2023-08-15 15:21:24 +0900

    :pencil: Design Login., Register.js와 usermodule.scss 추가

--- front/src/components/user/Login.js
+++ front/src/components/user/Login.js
@@ -1,12 +1,17 @@
 import styles from "./user.module.scss";
-
+import React, { useState } from "react";
 const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  }
   return (
     <form className={styles.container}>
       <label>id</label>
-      <input className="wrong" type="text" placeholder="id" />
+      <input className="id" type="email" placeholder="user@example.com" />
       <label>pw</label>
-      <input type="password" placeholder="pw" />
+      <input type="password" placeholder="비밀번호" />
     </form>
   );
 };
