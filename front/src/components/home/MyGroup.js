bc6d64d4414199651696a5a3f1c1766448dbb07f
a-honey <jungahyeon0512@khu.ac.kr>
2023-08-15 18:44:07 +0900

    :lipstick: Design: 배치 제외 홈 페이지 컴포넌트 스타일링 추가

--- front/src/components/home/MyGroup.js
+++ front/src/components/home/MyGroup.js
@@ -3,7 +3,10 @@ import styles from './index.module.scss';
 const MyGroup = () => {
   return (
     <div className={styles.myGroup}>
-      <h1>나의 모임 현황</h1>
+      <div className={styles.titleContainer}>
+        <h1>나의 모임 현황</h1>
+        <button className={styles.btn}>모임 리스트 관리</button>
+      </div>
       <div className={styles.contentContainer}>모임 넣을겁니다.</div>
     </div>
   );
--- front/src/components/home/MyUser.js
+++ front/src/components/home/MyUser.js
@@ -3,7 +3,10 @@ import styles from './index.module.scss';
 const MyUser = () => {
   return (
     <div className={styles.myUser}>
-      <h1>나의 친구들 현황</h1>
+      <div className={styles.titleContainer}>
+        <h1>나의 친구들 현황</h1>
+        <button className={styles.btn}>친구 관리</button>
+      </div>
       <div className={styles.contentContainer}>친구들 넣을겁니다.</div>
     </div>
   );
