8b0014be1f3aeb213217a00e69a4920ec52474b1
[AI8/1반] 지영은 <utael706@gmail.com>
2023-08-15 18:58:13 +0900

    :pencil: Style Login.js 추가

--- front/src/components/user/Login.js
+++ front/src/components/user/Login.js
@@ -10,8 +10,7 @@ const Login = () => {
   const [password, setPassword] = useState("");
   //useState로 confirmPassword 상태를 생성함.
   const [confirmPassword, setConfirmPassword] = useState("");
-  //useState로 name 상태를 생성함.
-  const [name, setName] = useState("");
+
 
   //이메일이 abc@example.com 형태인지 regex를 이용해 확인함.
   const validateEmail = (email) => {
@@ -28,23 +27,27 @@ const Login = () => {
   const isPasswordValid = password.length >= 4;
   // 비밀번호와 확인용 비밀번호가 일치하는지 여부를 확인함.
   const isPasswordSame = password === confirmPassword;
-  // 이름이 2글자 이상인지 여부를 확인함.
-  const isNameValid = name.length >= 2;
 
   // 위 4개 조건이 모두 동시에 만족되는지 여부를 확인함.
-  const isFormValid = isEmailValid && isPasswordValid && isPasswordSame && isNameValid;
-  if (!isFormValid) {
-    return alert("에러러ㅓ")
+  const isFormValid = isEmailValid && isPasswordValid && isPasswordSame;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!isFormValid) {
+
+    }
   }
   return (
     <form className={styles.container}>
       <label>아이디</label>
-      <input className="id" type="email" placeholder="user@example.com" />
+      <input className="id" type="email" placeholder="user@example.com" onChange={(e) => setEmail(e.target.value)} />
+      {!isEmailValid && <div className={styles["error-message"]}>유효한 이메일 주소를 입력하세요.</div>}
+
       <label>비밀번호</label>
-      <input type="password" placeholder="비밀번호" />
-      <button type="submit"> 로그인 </button>
-      <button type="submit" className="kakaoLogin"> 카카오 로그인</button>
-      <button type="submit" className="googleLogin"> 카카오 로그인</button>
+      <input type="password" placeholder="비밀번호" onChange={(e) => setPassword(e.target.value)} />
+      {!isPasswordValid && <div className={styles["error-message"]}>비밀번호는 4글자 이상이어야 합니다.</div>}
+
+      <button type="submit" onSubmit={handleSubmit}>로그인 </button>
 
     </form>
   );
