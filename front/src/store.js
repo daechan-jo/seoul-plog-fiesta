1c9e89ffa4ee91ba2e2f370a6e6ebe5a370fb0bb
a-honey <jungahyeon0512@khu.ac.kr>
2023-08-16 15:40:15 +0900

    :books: Docs: 리덕스 툴킷 설치 및 경로 변경

--- front/src/store.js
+++ front/src/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userReducer from '../features/userSlice';
+import userReducer from './features/user/userSlice';
 
 export const store = configureStore({
   reducer: {
