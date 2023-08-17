703d4226c79fdc40f3348f77079de71bd4bfcf90
a-honey <jungahyeon0512@khu.ac.kr>
2023-08-17 19:22:43 +0900

    :pencil: Chore: d3 라이브러리 설명 주석 추가

--- front/src/common/components/common/Map.js
+++ front/src/common/components/common/Map.js
@@ -31,31 +31,37 @@ const mockData = {
 };
 
 const Map = () => {
+  // svg 요소에 지도를 그리기 위해 참조를 생성함
   const svgRef = useRef();
 
+  // 렌더링되었을 때 데이터를 불러와서 지도를 그림
   useEffect(() => {
+    // 현재 참조된 요소에 지도를 그리겠다고 선언함
     const svg = d3.select(svgRef.current);
 
     const width = 800;
     const height = 600;
 
+    // 메르카토르 투영을 생성 후, 사이즈 조절 및 geojson 파일을 투영할 것을 설정함
     const projection = d3.geoMercator().fitSize([width, height], geojson);
 
+    // 지도 경로 생성 설정을 생성 후, 투영함
     const pathGenerator = d3.geoPath().projection(projection);
 
+    // 색상 보간(시작 색상에서 끝 색상으로 점진적으로 변하는 색상)을 호출 후 이 값을 연속값 매핑 함수를 호출하여 0~10으로 매핑
     const colorScale = d3.scaleSequential(d3.interpolateGreens).domain([0, 10]);
 
     svg
       .selectAll('path')
       .data(geojson.features)
       .enter()
-      .append('path')
+      .append('path') // 모든 지역구를 추가함
       .attr('d', pathGenerator)
       .style('fill', (d) => {
         const districtName = d.properties.name_eng
           .replace(/-gu/g, '')
-          .toLowerCase();
-        return colorScale(mockData[districtName] || 0);
+          .toLowerCase(); // 현재 geojson의 지역 이름을 조정함
+        return colorScale(mockData[districtName] || 0); // 지역구의 값에 따라서 색상을 결정함
       })
       .style('stroke', 'white');
   }, []);
