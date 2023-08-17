9c31ef5ed922132773cb08be8df27372c26ece5c
JoDaeChan <daechan476@gmail.com>
2023-08-18 02:11:14 +0900

    :ambulance: Fix: userRouter fix

--- back/index.js
+++ back/index.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { app } from "./src/app";
 
-const PORT = process.env.SERVER_PORT || 3000; //todo 환경변수 추가
+const PORT = process.env.SERVER_PORT;
 app.listen(PORT, () => {
 	console.log(
 		`🚀::Server connection successful  |  http://localhost:${PORT}`,
