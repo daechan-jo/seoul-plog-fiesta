297ed303e64fa8343049b5a019f1eaf000b9159a
[AI8/1반] 지영은 <utael706@gmail.com>
2023-08-17 20:47:25 +0900

    :lipstick: Design: user.module.scss 조금 수정 및 프론트 README.md 작성

--- front/README.md
+++ front/README.md
@@ -5,44 +5,132 @@
 ```
 ┠─ node_modules
 ┠─ public
-┠─ src
-│    ├ components
-│    │    ├ common
-│    │    │    ├ header.js
-│    │    │    └ header.module.scss
-│    │    ├ main
-│    │    │    └ mainBox
-│    │    │         └ mainBox
-│    │    │              ├ index.module.scss
-│    │    │              └ index.module.scss
-│    │    └ user
-│    │         ├ Login.js
-│    │         ├ Register.js
-│    │         └ user.module.scss
-│    ├ containers
-│    │    ├ main
-│    │    │    └ index.js
-│    │    └ user
-│    │         └ index.js
-│    ├ pages
-│    │    ├ Layout.js
-│    │    ├ MainPage.js
-│    │    └ UserPage.js
-│    └ styles
-│         ├ global.scss
-│         └ variables.scss
-├ .eslintrc.json
-├ .gitignore
-├ next-env.d.ts
-├ next.config.js
-├ package.json
-├ postcss.config.js
-├ README.md
-├ tailwind.config.js
-├ tsconfig.json
-└ yarn.lock
+src
+ ┣ api
+ ┃ ┗ index.js
+ ┣ assets
+ ┃ ┗ seoul_municipalities_geo_simple.json
+ ┣ common
+ ┃ ┣ components
+ ┃ ┃ ┣ common
+ ┃ ┃ ┃ ┣ ErrorModal.js
+ ┃ ┃ ┃ ┣ index.module.scss
+ ┃ ┃ ┃ ┣ Map.js
+ ┃ ┃ ┃ ┣ PageNav.js
+ ┃ ┃ ┃ ┣ Plogging.js
+ ┃ ┃ ┃ ┗ PloggingForm.js
+ ┃ ┃ ┣ home
+ ┃ ┃ ┃ ┣ Map.js
+ ┃ ┃ ┃ ┣ MyGroup.js
+ ┃ ┃ ┃ ┗ MyUser.js
+ ┃ ┃ ┣ intro
+ ┃ ┃ ┃ ┣ Intro.js
+ ┃ ┃ ┃ ┗ intro.module.scss
+ ┃ ┃ ┣ layout
+ ┃ ┃ ┃ ┣ Header.js
+ ┃ ┃ ┃ ┣ layout.module.scss
+ ┃ ┃ ┃ ┗ Nav.js
+ ┃ ┃ ┣ my
+ ┃ ┃ ┃ ┣ index.module.scss
+ ┃ ┃ ┃ ┣ MyGroups.js
+ ┃ ┃ ┃ ┣ MyInfo.js
+ ┃ ┃ ┃ ┗ MyUsers.js
+ ┃ ┃ ┣ myNetwork
+ ┃ ┃ ┃ ┣ ItemList.js
+ ┃ ┃ ┃ ┣ Pagenation.js
+ ┃ ┃ ┃ ┗ search.js
+ ┃ ┃ ┣ network
+ ┃ ┃ ┃ ┗ ItemList.js
+ ┃ ┃ ┣ ranking
+ ┃ ┃ ┃ ┣ Map.js
+ ┃ ┃ ┃ ┣ TopGroup.js
+ ┃ ┃ ┃ ┗ TopUser.js
+ ┃ ┃ ┗ user
+ ┃ ┃ ┃ ┣ Login.js
+ ┃ ┃ ┃ ┣ PasswordChange.js
+ ┃ ┃ ┃ ┣ Register.js
+ ┃ ┃ ┃ ┗ user.module.scss
+ ┃ ┗ containers
+ ┃ ┃ ┣ home
+ ┃ ┃ ┃ ┗ index.js
+ ┃ ┃ ┣ intro
+ ┃ ┃ ┃ ┗ index.js
+ ┃ ┃ ┣ my
+ ┃ ┃ ┃ ┣ index.js
+ ┃ ┃ ┃ ┗ index.module.scss
+ ┃ ┃ ┣ myNetwork
+ ┃ ┃ ┃ ┗ index.js
+ ┃ ┃ ┣ network
+ ┃ ┃ ┃ ┗ index.js
+ ┃ ┃ ┣ password
+ ┃ ┃ ┃ ┗ index.js
+ ┃ ┃ ┣ ranking
+ ┃ ┃ ┃ ┗ index.js
+ ┃ ┃ ┗ user
+ ┃ ┃ ┃ ┗ index.js
+ ┣ features
+ ┃ ┗ user
+ ┃ ┃ ┗ userSlice.js
+ ┣ pages
+ ┃ ┣ HomePage.js
+ ┃ ┣ IntroPage.js
+ ┃ ┣ Layout.js
+ ┃ ┣ MyNetworkPage.js
+ ┃ ┣ MyPage.js
+ ┃ ┣ NetworkPage.js
+ ┃ ┣ PasswordPage.js
+ ┃ ┣ RankingPage.js
+ ┃ ┗ UserPage.js
+ ┣ styles
+ ┃ ┣ global.scss
+ ┃ ┣ mystyle.css
+ ┃ ┣ reset.css
+ ┃ ┗ variables.scss
+ ┣ utils
+ ┃ ┗ index.js
+ ┣ App.js
+ ┣ index.js
+ ┗ store.js
 ```
-
+- api 폴더
+- assets폴더
+- common폴더:componets와 containers를 state랑 분리하려고 common으로 묶
+    - components 폴더 -스타일링 중심,view를 그리는 컴포넌트가 포함되어있습니다.
+        - common -ErrorModal/Map/PageNav/Plogging/PloggingForm
+        - home -Map/MyGroup/MyUser
+        - intro-Intro
+        - layout-Header/Nav , layout.module.scss
+        - my -MyGroups/MyInfo/MyUsers
+        - myNetwork -ItemList/Pagenation/search
+        - network -ItemList
+        - ranking -Map/TopGroup/TopUser
+        - user -Login/PasswordChange/Register,user.module.scss
+    - containers 폴더 -요청 중심 로직, 각 index.js에 view를 나타내는 컴포넌트가 포함되어있습니다.
+        - home: 메인 페이지
+        - intro: 인트로페이지(로그인정보 없을 경우 표시)
+        - my: 사용자 페이지
+        - myNetwork: 사용자의 모임,친구 표시
+        - network: 전체 모임 표시
+        - ranking: 유저/전체 사용자 랭킹 표시
+        - user:로그인 표시
+- features 폴더 -액션, 리듀서 등 상태관리. Redux Toolkit의 createSlice.reducer함수를 사용합니다.
+- pages-react router등을 이용하여 라우팅을 적용하는 페이지 컴포넌트, 컨테이너를 레이아웃으로 감싸 스타일을 적용시킴
+    - HomePage
+    - IntroPage
+    - Layout
+    - MyNetworkPage
+    - PasswordPage
+    - RankingPage
+    - UserPage
+- styles폴더
+    - global.scss
+    - mystyle.css
+    - reset.css
+    - variable.scss
+- utils 폴더
+- App.js - 라우팅 코드
+- index.js- 리덕스를 사용하는 리액트 렌더링 코드
+- store.js - 리덕스 Toolkit의 configureStorage함수를 사용해 스토어를 생성합니다.
 <br>
 
 ---
--- front/src/common/components/user/Login.js
+++ front/src/common/components/user/Login.js
@@ -3,6 +3,8 @@ import React, { useState, useContext } from "react";
 import { useNavigate, useHistory } from "react-router-dom";
 import { AiOutlineArrowLeft } from "react-icons/ai"
 //@ts-ignore
+// import * as Api from "../../api";
+// import { DispatchContext } from "../../App";
 
 const Login = () => {
   const navigate = useNavigate()
@@ -12,7 +14,7 @@ const Login = () => {
   const [password, setPassword] = useState("");
   //useState로 confirmPassword 상태를 생성함.
   const [confirmPassword, setConfirmPassword] = useState("");
-
+  // const dispatch = useContext(DispatchContext);
 
   //이메일이 abc@example.com 형태인지 regex를 이용해 확인함.
   const validateEmail = (email) => {
@@ -35,10 +37,32 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!isFormValid) {
 
-    }
-  }
+    //   try {
+    //     // "user/login" 엔드포인트로 post요청함.
+    //     const res = await Api.post("user/login", {
+    //       email,
+    //       password,
+    //     });
+    //     // 유저 정보는 response의 data임.
+    //     const user = res.data;
+    //     // JWT 토큰은 유저 정보의 token임.
+    //     const jwtToken = user.token;
+    //     // sessionStorage에 "userToken"이라는 키로 JWT 토큰을 저장함.
+    //     sessionStorage.setItem("userToken", jwtToken);
+    //     // dispatch 함수를 이용해 로그인 성공 상태로 만듦.
+    //     dispatch({
+    //       type: "LOGIN_SUCCESS",
+    //       payload: user,
+    //     });
+
+    //     // 기본 페이지로 이동함.
+    //     navigate("/", { replace: true });
+    //   } catch (err) {
+    //     console.log("로그인에 실패하였습니다.\n", err);
+    //   }
+  };
+
   const findPassword = () => {
     navigate("/setpassword")
   }
@@ -48,18 +72,18 @@ const Login = () => {
   };
 
   // //카카오톡 로그인을 위한 변수들
-  // const REST_API_KEY = '백엔드한테 달라하자1';
-  // const REDIRECT_URI = '백엔드한테 달라하자2';
-  // const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+  const REST_API_KEY = '백엔드한테 달라하자1';
+  const REDIRECT_URI = '백엔드한테 달라하자2';
+  const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
-  // // 카카오톡 로그인을 위한 loginHandler
-  // const kakaoLoginHandler = () => {
-  //   window.location.href = link;
-  // };
+  // 카카오톡 로그인을 위한 loginHandler
+  const kakaoLoginHandler = () => {
+    window.location.href = link;
+  };
 
   return (
-    <form className={styles.logincontainer}>
-      <AiOutlineArrowLeft onClick={goBack} />
+    <form className={styles.container}>
+      <AiOutlineArrowLeft className={styles.arrowLeft} onClick={goBack} />
       <label>아이디</label>
       <input className="id" type="email" placeholder="user@example.com" onChange={(e) => setEmail(e.target.value)} />
       {!isEmailValid && <div className={styles["error-message"]}>유효한 이메일 주소를 입력하세요.</div>}
@@ -67,11 +91,11 @@ const Login = () => {
       <label>비밀번호</label>
       <input type="password" placeholder="비밀번호" onChange={(e) => setPassword(e.target.value)} />
       {!isPasswordValid && <div className={styles["error-message"]}>비밀번호는 4글자 이상이어야 합니다.</div>}
-      <div className={styles.buttonContainer}>
-        <button className={styles.localLogin} type="submit" onSubmit={handleSubmit}>로그인 </button>
-        <button className={styles.localLogin} onClick={findPassword}>비밀번호 찾기</button>
-        {/* <button className={styles.kakaoLogin} onClick={kakaoLoginHandler}>카카오 로그인</button> */}
-      </div>
+      {/* <div className={styles.buttonContainer}> */}
+      <button className={styles.localLogin} type="submit" onSubmit={handleSubmit}>로그인 </button>
+      <button className={styles.localLogin} onClick={findPassword}>비밀번호 찾기</button>
+      <button className={styles.kakaoLogin} onClick={kakaoLoginHandler}>카카오 로그인</button>
+      {/* </div> */}
     </form>
 
 
