063b85389f583a5874a5579176e3d73afcec21b2
a-honey <jungahyeon0512@khu.ac.kr>
2023-08-18 11:11:08 +0900

    :pencil: Chore: 목업데이터 추가

--- front/src/common/containers/myNetwork/index.js
+++ front/src/common/containers/myNetwork/index.js
@@ -1,16 +1,43 @@
 import { useState } from 'react';
-import PageNav from "../../components/common/PageNav";
-import ItemList from "../../components/myNetwork/ItemList";
+import PageNav from '../../components/common/PageNav';
+import ItemList from '../../components/myNetwork/ItemList';
 
 const MyNetworkContainer = () => {
-  const [lists, setLists] = useState(['groups', 'users'])
+  const [lists, setLists] = useState(['groups', 'users']);
 
   return (
     <main>
       <PageNav lists={lists} setLists={setLists} />
-      <ItemList/>
+      <ItemList />
     </main>
   );
 };
 
 export default MyNetworkContainer;
+
+// view 따라 다른 api를 요청하여 내 그룹 혹은 유저 리스트를 담아서 ItemList에 전달
+const mockupUser = [
+  { id: '1', name: '유저이름1', region: 'gwangin' },
+  { id: '2', name: '유저이름2', region: 'dongdaemun' },
+  { id: '3', name: '유저이름3', region: 'gangnam' },
+  { id: '4', name: '유저이름4', region: 'mapo' },
+  { id: '5', name: '유저이름5', region: 'jongno' },
+  { id: '6', name: '유저이름6', region: 'yongsan' },
+  { id: '7', name: '유저이름7', region: 'gangbuk' },
+  { id: '8', name: '유저이름8', region: 'songpa' },
+  { id: '9', name: '유저이름9', region: 'seongdong' },
+  { id: '10', name: '유저이름10', region: 'seodaemun' },
+];
+
+const mockupGroup = [
+  { id: '1', name: '그룹이름1', region: 'gwangin' },
+  { id: '2', name: '그룹이름2', region: 'dongdaemun' },
+  { id: '3', name: '그룹이름3', region: 'gangnam' },
+  { id: '4', name: '그룹이름4', region: 'mapo' },
+  { id: '5', name: '그룹이름5', region: 'jongno' },
+  { id: '6', name: '그룹이름6', region: 'yongsan' },
+  { id: '7', name: '그룹이름7', region: 'gangbuk' },
+  { id: '8', name: '그룹이름8', region: 'songpa' },
+  { id: '9', name: '그룹이름9', region: 'seongdong' },
+  { id: '10', name: '그룹이름10', region: 'seodaemun' },
+];
--- front/src/common/containers/ranking/index.js
+++ front/src/common/containers/ranking/index.js
@@ -17,3 +17,20 @@ const HomeContainer = () => {
 };
 
 export default HomeContainer;
+
+// 상위 스코어 5명을 가져옴
+const mockupGroup = [
+  { id: '1', name: '모임이름1', score: 100 },
+  { id: '2', name: '모임이름2', score: 150 },
+  { id: '3', name: '모임이름3', score: 80 },
+  { id: '4', name: '모임이름4', score: 200 },
+  { id: '5', name: '모임이름5', score: 120 },
+];
+
+const mockupUser = [
+  { id: '1', name: '유저이름1', score: 250 },
+  { id: '2', name: '유저이름2', score: 180 },
+  { id: '3', name: '유저이름3', score: 300 },
+  { id: '4', name: '유저이름4', score: 150 },
+  { id: '5', name: '유저이름5', score: 220 },
+];
