9886caee9d5640fc90f0f3eb463bc1e2cf5f8c25
a-honey <jungahyeon0512@khu.ac.kr>
2023-08-18 12:29:59 +0900

    :sparkles: Feat: query에 따른 네트워크페이지 재렌더링 추가
     useLocation에서 query 값을 가져옴 + 목업데이터 스타일링없이 렌더링

--- front/src/common/components/ranking/TopUser.js
+++ front/src/common/components/ranking/TopUser.js
@@ -1,10 +1,16 @@
-const TopUser = () => {
+import Item from './Item';
+
+const TopUser = ({ datas }) => {
   return (
     <div className="gContainer">
       <div className="titleContainer">
         <h1>상위 유저</h1>
       </div>
-      <div className="contentMinContainer">친구들 넣을겁니다.</div>
+      <div className="contentListContainer">
+        {datas.map((data) => (
+          <Item data={data} />
+        ))}
+      </div>
     </div>
   );
 };
