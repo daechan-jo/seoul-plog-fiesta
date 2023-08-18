b1012109b61a923b047d4fa90d1962d0e6d0a463
a-honey <jungahyeon0512@khu.ac.kr>
2023-08-19 02:29:14 +0900

    :art: Feat: axios api 구현 및 로그인 구현
    form input 오류로 테스트하지 않음

--- front/src/common/components/myNetwork/ItemList.js
+++ front/src/common/components/myNetwork/ItemList.js
@@ -1,11 +1,15 @@
 import Item from '../network/Item';
 import Paging from './Pagenation';
+import { BiSearchAlt2 } from 'react-icons/bi';
 
 const ItemList = ({ datas }) => {
   return (
     <div className="gContainer gList">
       <div className="titleContainer">
-        <h1>나의 친구들</h1>
+        <h1>
+          나의 친구들
+          <BiSearchAlt2 />
+        </h1>
       </div>
       <div className="contentListContainer">
         {datas.map((data) => (
