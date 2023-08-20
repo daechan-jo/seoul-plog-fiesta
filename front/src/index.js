e9cbbb7f582c999d27b6217b1679783004c0dcb7
a-honey <jungahyeon0512@khu.ac.kr>
2023-08-20 23:25:42 +0900

    :package: Chore: RecoilRoot 추가

--- front/src/index.js
+++ front/src/index.js
@@ -7,14 +7,17 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <RecoilRoot>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </RecoilRoot>
     </Provider>
   </React.StrictMode>,
 );
