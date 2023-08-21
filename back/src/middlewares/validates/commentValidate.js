a193df21c36b729bbe4939c7a6a7a2bd7cb212c0
JoDaeChan <daechan476@gmail.com>
2023-08-21 23:33:43 +0900

    :sparkles: Feat: 댓글 유효성 검사 추가
    댓글 작성, 댓글 업데이트 유효성 검사 추가

--- back/src/middlewares/validates/commentValidate.js
+++ back/src/middlewares/validates/commentValidate.js
@@ -1,18 +1,25 @@
 const Joi = require("joi");
 
-// const createGroupSchema = Joi.object({
-// 	name: Joi.string().min(1).max(50).required(),
-// 	goal: Joi.string().min(1).max(1000).optional().allow(""),
-// 	region: Joi.string().min(1).max(1000).optional().allow(""),
-// 	introduction: Joi.string().min(1).max(1000).optional().allow(""),
-// });
-// const validateGroupCreation = (req, res, next) => {
-// 	const validGroup = createGroupSchema.validate(req.body);
-// 	if (validGroup.error) {
-// 		return res.status(400).json({
-// 			message: "유효성 검사 실패.",
-// 			details: validGroup.error.details,
-// 		});
-// 	}
-// 	next();
-// };
+const createCommentSchema = Joi.object({
+	content: Joi.string().min(1).max(200).required().messages({
+		"string.base": "내용이 문자열이 아닙니다.",
+		"string.min": "내용이 너무 짧습니다.",
+		"string.max": "내용이 너무 깁니다.",
+		"any.required": "내용이 없습니다.",
+	}),
+	parentId: Joi.number().optional().messages({
+		"number.base": "부모댓글이 정수가 아닙니다.",
+	}),
+});
+const validateCommentCreation = (req, res, next) => {
+	const validGroup = createCommentSchema.validate(req.body);
+	if (validGroup.error) {
+		return res.status(400).json({
+			message: "유효성 검사 실패.",
+			details: validGroup.error.details,
+		});
+	}
+	next();
+};
+
+module.exports = { validateCommentCreation };
