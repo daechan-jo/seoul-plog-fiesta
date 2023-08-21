f68d4dfc35a0b568c783ad622ec4d9313fd5d26d
a-honey <jungahyeon0512@khu.ac.kr>
2023-08-21 20:14:48 +0900

    :sparkles: Feat: 그룹 페이지 pageNav 설정

---
+++ front/src/components/groupId/Notice.js
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import * as Api from '../../api';
+
+const Notice = ({ view }) => {
+  const [isFetching, setIsFetching] = useState(false);
+  const [datas, setDatas] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setIsFetching(true);
+        const res = await Api.get(``);
+        //setDatas(res.data);
+      } catch (err) {
+        console.log('공지사항 데이터를 불러오는데 실패.', err);
+      } finally {
+        setIsFetching(false);
+      }
+    };
+
+    getData();
+  }, [view]);
+
+  return (
+    <div className="gContainer  gList navVh">
+      <div className="titleContainer">
+        <h1>그룹게시판</h1>
+      </div>
+      <div className="contentListContainer">
+        {isFetching ? (
+          <div>로딩중</div>
+        ) : datas?.length === 0 ? (
+          <div>데이터가 없습니다.</div>
+        ) : (
+          datas.map((data) => <Item data={data} key={data.id} view={view} />)
+        )}
+      </div>
+      <div>페이지네이션자리</div>
+    </div>
+  );
+};
+
+export default Notice;
+
+const Item = ({ data, view }) => {
+  const navigator = useNavigate();
+  const [isModal, setIsModal] = useState(false);
+
+  return (
+    <div
+      onClick={() => {
+        setIsModal(true);
+      }}
+    >
+      {isModal && <PostItem id={data.id} setIsModal={setIsModal} />}
+      <div>나의 인증1</div>
+      <div>장소</div>
+    </div>
+  );
+};
+
+const PostItem = ({ id, setIsModal }) => {
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setIsFetching(true);
+        const res = await Api.get(``);
+        //setDatas(res.data);
+      } catch (err) {
+        console.log('공지사항 데이터를 불러오는데 실패.', err);
+      } finally {
+        setIsFetching(false);
+      }
+    };
+
+    getData();
+  }, []);
+
+  return (
+    <div className="modal">
+      <div>나의 인증1</div>
+      <div>장소</div>
+      <button>back</button>
+      <button>뒤로가기</button>
+    </div>
+  );
+};
--- front/src/components/groupId/Posts.js
+++ front/src/components/groupId/Posts.js
@@ -1,3 +1,6 @@
+import { useEffect, useState } from 'react';
+import * as Api from '../../api';
+
 const GroupPosts = () => {
   return (
     <div className="gContainer">
@@ -5,9 +8,48 @@ const GroupPosts = () => {
         <h1>공지사항</h1>
         <button className="gBtn">게시판 바로가기</button>
       </div>
-      <div className="contentContainer">공지사항만 넣을겁니다.</div>
+      <List />
     </div>
   );
 };
 
 export default GroupPosts;
+
+const List = () => {
+  const [datas, setDatas] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setIsFetching(true);
+        const res = await Api.get('');
+        //setDatas(res.data);
+      } catch (err) {
+        console.log('그룹공지사항데이터불러오기 실패', err);
+      } finally {
+        setIsFetching(false);
+      }
+    };
+
+    getData();
+  }, []);
+
+  return (
+    <div className="contentContainer">
+      {isFetching ? (
+        <div>로딩중</div>
+      ) : datas.length === 0 ? (
+        <div>데이터가 없습니다</div>
+      ) : (
+        datas.map((data) => (
+          <Item key={`group_page ${data.groupId}`} data={data} />
+        ))
+      )}
+    </div>
+  );
+};
+
+const Item = ({ data }) => {
+  return <div>data입니다.</div>;
+};
