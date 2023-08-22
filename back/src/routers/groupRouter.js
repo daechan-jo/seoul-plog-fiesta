9a26d646829393b91a540289b01171a32cc53401
JoDaeChan <daechan476@gmail.com>
2023-08-22 17:14:57 +0900

    :hammer: Refactor: groupList
    그룹리스트에 임시로 이미지url 추가
    이미지 업로드&로드 경로 src 내부로 변경

--- back/src/routers/groupRouter.js
+++ back/src/routers/groupRouter.js
@@ -1,90 +1,90 @@
-import router from "express";
-import authenticateJWT from "../middlewares/authenticateJWT";
-import groupController from "../controllers/groupController";
-import groupValidate from "../middlewares/validates/groupValidate";
+import router from 'express';
+import authenticateJWT from '../middlewares/authenticateJWT';
+import groupController from '../controllers/groupController';
+import groupValidate from '../middlewares/validates/groupValidate';
 const groupRouter = router();
 
 /** @description 그룹 생성 */
 groupRouter.post(
-	"/group",
+	'/group',
 	authenticateJWT,
 	groupValidate.validateGroupCreation,
 	groupController.createGroup,
 );
 
 /** @description 모든 그룹 조회 */
-groupRouter.get("/group", groupController.getAllGroups);
+groupRouter.get('/group', groupController.getAllGroups);
 
 /** @description 그룹 가입 신청 */
 groupRouter.post(
-	"/group/join/:groupid",
+	'/group/join/:groupid',
 	authenticateJWT,
 	groupController.requestToJoinGroup,
 );
 
 /** @description 그룹 가입 신청 목록 조회 */
 groupRouter.get(
-	"/group/join",
+	'/group/join',
 	authenticateJWT,
 	groupController.getGroupJoinRequests,
 );
 
 /** @description 그룹 가입 신청 수락 */
 groupRouter.post(
-	"/group/accept/:groupid/:userid",
+	'/group/accept/:groupid/:userid',
 	authenticateJWT,
 	groupController.acceptRegistration,
 );
 
 /** @description 그룹 가입 신청 거절 */
 groupRouter.delete(
-	"/group/reject/:groupid/:userid",
+	'/group/reject/:groupid/:userid',
 	authenticateJWT,
 	groupController.rejectGroupJoinRequest,
 );
 
 /** @description 내가 가입한 그룹 조회 */
-groupRouter.get("/group/mygroup", authenticateJWT, groupController.getMyGroups);
+groupRouter.get('/group/mygroup', authenticateJWT, groupController.getMyGroups);
 
 /** @description 그룹 상세정보 */
 groupRouter.get(
-	"/group/:groupid",
+	'/group/:groupid',
 	authenticateJWT,
 	groupController.getGroupDetails,
 );
 
 /** @description 그룹 게시글 작성 */
 groupRouter.post(
-	"/group/post/:groupid",
+	'/group/post/:groupid',
 	authenticateJWT,
 	groupValidate.validateGroupPostCreation,
 	groupController.createPost,
 );
 
-/** @description 해당 게시글 전체 목록 */
+/** @description 그룹아이디 해당 게시글 전체 목록 */
 groupRouter.get(
-	"/group/posts/:groupid",
+	'/group/posts/:groupid',
 	authenticateJWT,
 	groupController.getAllPosts,
 );
 
 /** @description 해당 게시글 상세정보 */
 groupRouter.get(
-	"/group/post/:postid",
+	'/group/post/:postid',
 	authenticateJWT,
 	groupController.getPostById,
 );
 
 /** @description 내가 속한 그룹 최근 게시글 목록 */
 groupRouter.get(
-	"/group/recent/posts",
+	'/group/recent/posts',
 	authenticateJWT,
 	groupController.getRecentPosts,
 );
 
 /** @description 그룹 게시글 수정 */
 groupRouter.put(
-	"/group/post/put/:postid",
+	'/group/post/put/:postid',
 	groupValidate.validateGroupPostUpdate,
 	authenticateJWT,
 	groupController.editPost,
@@ -92,28 +92,28 @@ groupRouter.put(
 
 /** @description 그룹 게시글 삭제 */
 groupRouter.delete(
-	"/group/post/delete/:postid",
+	'/group/post/delete/:postid',
 	authenticateJWT,
 	groupController.deletePost,
 );
 
 /** @description 그룹 탈퇴 */
 groupRouter.delete(
-	"/group/:groupid",
+	'/group/:groupid',
 	authenticateJWT,
 	groupController.leaveGroup,
 );
 
 /** @description 그룹원 추방 */
 groupRouter.delete(
-	"/group/expulse/:groupid/:userid",
+	'/group/expulse/:groupid/:userid',
 	authenticateJWT,
 	groupController.removeGroupMember,
 );
 
 /** @description 그룹 삭제 */
 groupRouter.delete(
-	"/group/drop/:groupid",
+	'/group/drop/:groupid',
 	authenticateJWT,
 	groupController.dropGroup,
 );
