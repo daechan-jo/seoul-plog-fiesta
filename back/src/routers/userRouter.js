6f838e4c217cb9de536477a99f77939035897524
[AI8기/1반] 최은진 <choiej.3190@gmail.com>
2023-08-22 16:51:18 +0900

    :sparkles: Feat: userFriendFunction

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -1,15 +1,54 @@
 import router from "express";
 import userController from "../controllers/userController";
+import authenticateJWT from "../middlewares/authenticateJWT";
 const userRouter = router();
 
-userRouter.get("/user", userController.currentUser);
+/** @description 모든 유저 정보 */
+userRouter
+    .get("/users",
+        userController.getAllUsers);
 
-// userRouter.get("/user/:name", userController.searchUsers);
 
-userRouter.delete("/user/drop", userController.deleteMyfriendship);
+/** @description 랜덤 유저 */
+userRouter
+    .get("/user/random",
+        userController.getRandomUsers);
 
-userRouter.get("/user/list", userController.getMyFriendship);
 
-userRouter.get("/user/random", userController.getRandomUsers);
+/** @description 유저 찾기 */
+userRouter
+    .get("/user/:name",
+        authenticateJWT,
+        userController.searchUsers);
+
+
+/** @description 현재 사용자 */
+userRouter
+    .get("/user",
+        authenticateJWT,
+        userController.currentUser);
+
+
+/** @description 친구 추가 */
+userRouter
+    .post("/user/add/:id",
+        authenticateJWT,
+        userController.addAsFriend);
+
+
+/** @description 친구 목록 */
+userRouter
+    .get("/user/friends",
+        authenticateJWT,
+        userController.getMyFriends);
+
+
+/** @description 친구 삭제 */
+userRouter
+    .delete("/user/drop/:id",
+        authenticateJWT,
+        userController.deleteFriend);
+
+
 
 module.exports = userRouter;
--- back/src/services/userService.js
+++ back/src/services/userService.js
@@ -1,93 +1,138 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-// /** @description 모든 유저 정보 */
-// const getAllUsers = async () => {
-//     try{
-//         return await prisma.user.findMany();
-//     } catch (e) {
-//         throw error("에러다 에러!");
-//     }
-// };
-
-//todo 넘겨받는값이 없음ㅎㅎ;
-/** @description 유저 찾기 */
-// const searchUsers = async () => {
-// 	try {
-// 		return await prisma.user.findMany({
-// 			where: {
-// 				OR: [
-// 					{ id: userId },
-// 					{ name: userName },
-// 					{ nickname: userNickname },
-// 				],
-// 			},
-// 		});
-// 	} catch (error) {
-// 		throw error("에러다 에러!");
-// 	}
-// };
-
-//todo 아몰랑 ㅎㅎ
-const getRandomUsers = async () => {
-	try {
-		return await prisma.user.findMany({
-			take: 1,
-			orderBy: { id: "desc" },
+/** @description 모든 유저 정보 */
+const getAllUsers = async () => {
+    try{
+        return await prisma.user.findMany({
 			select: {
+				id: true,
 				nickname: true,
+				about: true,
 				activity: true,
-				authCount: true,
+			},
+		});
+    } catch (error) {
+        throw error;
+    }
+};
+
+
+/** @description 유저 찾기 */
+const searchUsers = async (nickname) => {
+	try {
+		return await prisma.user.findUnique({
+			where: {
+				nickname: nickname,
+			},
+			select: {
+				id: true,
+				nickname: true,
+				about: true,
+				activity: true,
+				// authCount: true,
 			},
 		});
 	} catch (error) {
-		throw error("에러다 에러!");
+		throw error;
 	}
 };
+
+/** @description 랜덤 유저 */
+const getRandomUsers = async () => {
+	try {
+		const randomCount = await prisma.user.count();
+		const skip = Math.floor(Math.random() * randomCount);
+		return await prisma.user.findMany({
+		take: 3,
+		skip: skip,
+		orderBy: {
+				id: "desc",
+			},
+		select: {
+			id: true,
+			nickname: true,
+			about: true,
+			activity: true,
+			},
+		});
+	} catch (error) {
+		throw error;
+	}
+};
+
 
 /** @description 유저 정보 */
 const getUserInfo = async (userId) => {
 	try {
-		return await prisma.user.findUnique(userId);
+		return await prisma.user.findUnique({
+			where: {
+				id: userId,
+			}
+		});
 	} catch (error) {
-		throw error("에러다 에러!");
+		throw error;
 	}
 };
 
-const getMyFriendship = async (friendshipId) => {
+
+
+/** @description 친구 추가 */
+const addAsFriend = async (userAId, userBId ) => {
 	try {
-		return await prisma.friendship.findUnique({
-			where: {
-				id: friendshipId,
-			},
-			include: {
-				AND: [{ id: userAId }, { id: userBId }],
+		await prisma.friendship.create({
+			data: {
+					userAId,
+					userBId,
+					},
+		});
+		return addAsFriend;
+	} catch (error) {
+		throw error;
+	}
+}
+
+
+/** @description 친구 목록 */
+const getMyFriends = async (userId) => {
+	try {
+		return await prisma.friendship.findMany({
+
+			select: {
+				userBId: true,
 			},
 		});
 	} catch (error) {
-		throw error("에러다 에러!");
+		throw error;
 	}
 };
 
-const deleteMyfriendship = async (friendshipId) => {
+
+/** @description 친구 삭제 */
+const deleteFriend = async (userAId, userBId) => {
 	try {
 		return await prisma.friendship.delete({
 			where: {
-				id: friendshipId,
-			},
-			include: {
-				AND: [{ id: userAId }, { id: userBId }],
+					userAId_userBId: {
+						userAId: userAId,
+						userBId: userBId,
+					},
 			},
 		});
 	} catch (error) {
-		throw error("에러다 에러!");
+		throw error;
 	}
 };
 
+/** @description 친구의 최신 게시물 */
+
+
 module.exports = {
-	// searchUsers,
+	getAllUsers,
+	searchUsers,
 	getUserInfo,
-	getMyFriendship,
-	deleteMyfriendship,
+	addAsFriend,
+	getMyFriends,
+	deleteFriend,
 	getRandomUsers,
 };
