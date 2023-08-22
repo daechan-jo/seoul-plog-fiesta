31690429ba50fcaec4ef1243f959cd8a7b26d1c6
JoDaeChan <daechan476@gmail.com>
2023-08-22 16:38:45 +0900

    :hammer: Refactor: groupList
    그룹리스트에 임시로 이미지url 추가
    이미지 업로드&로드 경로 src 내부로 변경

--- back/src/app.js
+++ back/src/app.js
@@ -1,30 +1,30 @@
-import cors from "cors";
-import express from "express";
-import cookieParser from "cookie-parser";
-import errorMiddleware from "./middlewares/errorMiddleware";
-import loggerMiddleware from "./middlewares/loggerMiddleware";
-import authRoutes from "./routers/authRouter";
-import userRoutes from "./routers/userRouter";
-import groupRoutes from "./routers/groupRouter";
-import uploadRouter from "./routers/uploadRouter";
-import loadRouter from "./routers/loadRouter";
-import commentRouter from "./routers/commentRouter";
-import chatRoutes from "./routers/chatRouter";
+import cors from 'cors';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import errorMiddleware from './middlewares/errorMiddleware';
+import loggerMiddleware from './middlewares/loggerMiddleware';
+import authRoutes from './routers/authRouter';
+import userRoutes from './routers/userRouter';
+import groupRoutes from './routers/groupRouter';
+import uploadRouter from './routers/uploadRouter';
+import loadRouter from './routers/loadRouter';
+import commentRouter from './routers/commentRouter';
+import chatRoutes from './routers/chatRouter';
 
-import http from "http";
-import socketIO from "socket.io";
+import http from 'http';
+import socketIO from 'socket.io';
 
-import { local, jwt } from "./config";
-const { PrismaClient } = require("@prisma/client");
+import { local, jwt } from './config';
+const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
-const passport = require("passport");
+const passport = require('passport');
 const app = express();
 
 const server = http.createServer(app);
 const io = socketIO(server);
 
-passport.use("local", local);
-passport.use("jwt", jwt);
+passport.use('local', local);
+passport.use('jwt', jwt);
 
 app.use(cors());
 app.use(express.json());
@@ -32,6 +32,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(loggerMiddleware);
 app.use(passport.initialize());
+// app.use(
+// 	'/uploads/images',
+// 	express.static(path.join(__dirname, '..', 'uploads', 'images')),
+// );
 
 app.use(authRoutes);
 app.use(userRoutes);
@@ -44,7 +48,7 @@ app.use(chatRoutes(io));
 app.use(errorMiddleware);
 
 /** @description 프로세스 종료 후 프리즈마 연결해제 */
-process.on("SIGINT", async () => {
+process.on('SIGINT', async () => {
 	await prisma.$disconnect();
 	process.exit();
 });
