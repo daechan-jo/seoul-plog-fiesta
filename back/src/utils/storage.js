9a1ca7cf873fea94c5147101e51f5382c20d29e6
JoDaeChan <daechan476@gmail.com>
2023-08-23 00:39:10 +0900

    :tractor: Remove: 잘못된 upload 경로 삭제

--- back/src/utils/storage.js
+++ back/src/utils/storage.js
@@ -1,11 +1,10 @@
-const multer = require("multer");
-const { v4: uuidv4 } = require("uuid");
-// const path = require('path');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
 
 const storage = multer.diskStorage({
-	destination: "uploads/images",
+	destination: 'src/uploads/images',
 	filename: (req, file, callback) => {
-		const imageFormat = file.mimetype.split("/")[1];
+		const imageFormat = file.mimetype.split('/')[1];
 		let filename = `img_${uuidv4()}.${imageFormat}`;
 		callback(null, filename);
 	},
