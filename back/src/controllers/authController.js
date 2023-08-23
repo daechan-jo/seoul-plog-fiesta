4d2c41a8855d2aeee7a7db819b97a97f90ea1dd1
[AI8/1반] 정현수 <conjugate0519@gmail.com>
2023-08-23 17:05:42 +0900

    ⚡️ Feat: 토큰 저장 기능 추가

--- back/src/controllers/authController.js
+++ back/src/controllers/authController.js
@@ -1,104 +1,109 @@
-import authService from "../services/authService";
-import smtpTransport from "../config/sendEmail";
-import randomPassword from "../utils/randomPassword";
-import { text } from "express";
+import authService from '../services/authService';
+import smtpTransport from '../config/sendEmail';
+import randomPassword from '../utils/randomPassword';
+import { text } from 'express';
 
 const createUser = async (req, res, next) => {
-	try {
-		const userData = req.body;
-		if (userData.password !== userData.confirmPassword)
-			throw new Error("비밀번호 확인 불일치");
-		const user = await authService.createUser(userData);
-		console.log(user);
-		res.status(201).json(user);
-	} catch (error) {
-		console.error(error);
-		error.status = 500;
-		next(error);
-	}
+  try {
+    const userData = req.body;
+    if (userData.password !== userData.confirmPassword)
+      throw new Error('비밀번호 확인 불일치');
+    const user = await authService.createUser(userData);
+    console.log(user);
+    res.status(201).json(user);
+  } catch (error) {
+    console.error(error);
+    error.status = 500;
+    next(error);
+  }
 };
 
 const login = async (req, res, next) => {
-	try {
-		const user = {
-			id: req.user.id,
-			token: req.token,
-			email: req.user.email,
-			nickname: req.user.nickname,
-		};
-		console.log(user);
-		res.status(200).json(user);
-	} catch (error) {
-		console.error(error);
-		error.status = 500;
-		next(error);
-	}
+  try {
+    const user = {
+      id: req.user.id,
+      token: req.token,
+      email: req.user.email,
+      nickname: req.user.nickname,
+    };
+    console.log(user);
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    error.status = 500;
+    next(error);
+  }
 };
 
-const findPasswordByEmail = async(req, res, next) =>{
-	try {
-		const email = req.body.email;
-		const existingUser = await authService.getUserByEmail(email);
-		const password = randomPassword.createRandomPassword();
-		//console.log(existingUser);
+const findPasswordByEmail = async (req, res, next) => {
+  try {
+    const email = req.body.email;
+    const existingUser = await authService.getUserByEmail(email);
+    const token = randomPassword.createRandomPassword(); //todo 함수 명 변경
+    //console.log(existingUser);
 
-		const emailOptions = {
-			from: "qweasdzxc0210@naver.com",
-			to: email,
-			subject: "임시 비밀번호를 알려드립니다.",
-			html:
-			"<h1>파인애플피자에서 새로운 비밀번호를 알려드립니다.</h1>"+
-			"<h2>임시 비밀번호는 "+ password +"입니다.</h2>"+
-			'<h3 style="color:crimson;">임시 비밀번호로 로그인 하신 후, 비밀번호를 수정해주세요.</h3>'+
-			'<img src="http://file3.instiz.net/data/cached_img/upload/2021/10/01/11/fcd74ebb3fc06be634475b93911b0a7f.jpg">'
-		};
+    const emailOptions = {
+      from: 'qweasdzxc0210@naver.com',
+      to: email,
+      subject: '[SeoulPlogFiesta] 비밀번호 변경 안내',
+      html:
+        '<h2>안녕하세요. SeoulPlogFiesta입니다.</h2>' +
+        '<h2>고객님의 비밀번호 변경을 위해 아래의 링크를 클릭해주세요.</h2>' +
+        '<a href= "http://localhost:3000/passwordChange?token=' +
+        token +
+        '">비밀번호 재설정 링크<a>',
+    };
 
-		smtpTransport.sendMail(emailOptions, (err,info) =>{
-			if(err){
-				console.log(err);
-			} else{
-				console.log("성공적으로 이메일을 전송하였습니다.", info.response);
-				smtpTransport.close();
-			}
-		})
+    smtpTransport.sendMail(emailOptions, (err, info) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('성공적으로 이메일을 전송하였습니다.', info.response);
+        smtpTransport.close();
+      }
+    });
 
-		//해당 유저의 비밀번호를 임시 비밀번호로 변경
-		const user = authService.changePassword(email, password);
-		res.status(200).json(user); //빈 응답
+    const user = await authService.updateTokenByEmail(email, token);
+    //해당 유저의 비밀번호를 임시 비밀번호로 변경
+    //const user = authService.changePassword(email, token);
+    res.status(200).json(user); //빈 응답
+  } catch (error) {
+    console.error(error);
+    error.status = 500;
+    next(error);
+  }
+};
 
-	} catch(error){
-		console.error(error);
-		error.status= 500;
-		next(error);
-	}
-}
+const changeInformation = async (req, res, next) => {
+  try {
+    const userData = req.body;
+    const user = await authService.changeInformation(userData);
+    console.log(user);
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    error.status = 500;
+    next(error);
+  }
+};
 
+const removeUser = async (req, res, next) => {
+  try {
+    const id = req.user.id;
+    const user = await authService.removeUser(id);
+    console.log(user);
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    error.status = 500;
+    next(error);
+  }
+};
 
-const changeInformation = async (req, res, next) =>{
-	try {
-		const userData = req.body
-		const user = await authService.changeInformation(userData);
-		console.log(user)
-		res.status(200).json(user);
-	} catch(error){
-		console.error(error);
-		error.status = 500;
-		next(error);
-	}
-}
-
-const removeUser = async (req, res,next) => {
-	try {
-		const id = req.user.id;
-		const user = await authService.removeUser(id);
-		console.log(user);
-		res.status(200).json(user);
-	} catch(error){
-		console.error(error);
-		error.status = 500;
-		next(error);
-	}
-}
-
-
-module.exports = { createUser, login , findPasswordByEmail, changeInformation, removeUser };
+module.exports = {
+  createUser,
+  login,
+  findPasswordByEmail,
+  changeInformation,
+  removeUser,
+};
--- back/src/services/authService.js
+++ back/src/services/authService.js
@@ -5,101 +5,117 @@ const { error } = require('console');
 const { accessSync } = require('fs');
 
 const createUser = async (userData) => {
-	const { name, nickname, email, password } = userData;
-	const existingUser = await prisma.user.findUnique({
-		where: {
-			email: email,
-		},
-	});
-	const existingNickname = await prisma.user.findUnique({
-		where: {
-			nickname: nickname,
-		},
-	});
-	if (existingUser) throw new Error('이미 존재하는 이메일입니다.');
-	if (existingNickname) throw new Error('이미 존재하는 닉네임입니다.');
+  const { name, nickname, email, password } = userData;
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+  const existingNickname = await prisma.user.findUnique({
+    where: {
+      nickname: nickname,
+    },
+  });
+  if (existingUser) throw new Error('이미 존재하는 이메일입니다.');
+  if (existingNickname) throw new Error('이미 존재하는 닉네임입니다.');
 
-	const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-	const newUser = {
-		name,
-		nickname,
-		email,
-		password: hashedPassword,
-	};
-	try {
-		const createdNewUser = await prisma.user.create({
-			data: newUser,
-		});
-		createdNewUser.errorMessage = null;
-		return createdNewUser;
-	} catch (error) {
-		throw error;
-	}
+  const newUser = {
+    name,
+    nickname,
+    email,
+    password: hashedPassword,
+  };
+  try {
+    const createdNewUser = await prisma.user.create({
+      data: newUser,
+    });
+    createdNewUser.errorMessage = null;
+    return createdNewUser;
+  } catch (error) {
+    throw error;
+  }
 };
 
 const getUserByEmail = async (email) => {
-	const existingUser = await prisma.user.findUnique({
-		where: {
-			email: email,
-		},
-	});
-	if (!existingUser) throw new Error('존재하지 않는 사용자입니다.');
-	return existingUser;
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+  if (!existingUser) throw new Error('존재하지 않는 사용자입니다.');
+  return existingUser;
 };
 
-//모르는 사람이 이메일로만 비밀번호를 변경한다면 ? => 보안 조치 ?
+//이메일로 유저 찾아 유저의 토큰 업데이트
+const updateTokenByEmail = async (email, token) => {
+  try {
+    const user = await prisma.user.update({
+      where: {
+        email: email,
+      },
+      data: {
+        passwordToken: token,
+      },
+    });
+    console.log(user);
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
 const changePassword = async (email, password) => {
-	//비밀번호 변경
-	//console.log(email, password);
-	const hashedPassword = await bcrypt.hash(password, 10);
-	const updateUser = await prisma.user.update({
-		where: {
-			email: email,
-		},
-		data: {
-			password: hashedPassword,
-		},
-	});
-	return updateUser;
+  //비밀번호 변경
+  //console.log(email, password);
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const updateUser = await prisma.user.update({
+    where: {
+      email: email,
+    },
+    data: {
+      password: hashedPassword,
+    },
+  });
+  return updateUser;
 };
 
 const changeInformation = async (userData) => {
-	const { id, name, nickname, password, about, activity } = userData;
-	try {
-		if (!name || !nickname || !password)
-			throw new Error('필수값들을 입력해주세요');
-		const hashedPassword = await bcrypt.hash(password, 10);
-		//todo 현재 nickname은 유니크값이라 중복값이 없습니다
-		//id는 다른데 nickname이 같은 사람이 있는경우 : nickname이 같으면 id를 대조
-		//혹은 모든 사람들의 데이터중에서 nickname이 같은 경우를 찾나 ?
-		//이미 unique라 이 전에 오류를 던지는지 => 필요 없는 코드 ?
-		/*const sameNicknameUser = await prisma.user.findUnique({
-			where: {
-				id : {not:id} ,
-				nickname: nickname,
-			}
-		})
-		if (sameNicknameUser) throw new Error("이미 존재하는 닉네임입니다.");
-		*/
-		const updateUser = await prisma.user.update({
-			where: {
-				id: id,
-			},
-			data: {
-				name: name,
-				nickname: nickname,
-				password: hashedPassword,
-				about: about, //빈 값 허용
-				activity: activity, //빈 값 허용
-			},
-		});
-		console.log('유저의 정보를 업데이트했습니다');
-		return updateUser;
-	} catch (error) {
-		throw error;
-	}
-	/*
+  const { id, name, nickname, password, about, activity } = userData;
+  try {
+    if (!name || !nickname || !password)
+      throw new Error('필수값들을 입력해주세요');
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const sameNicknameUser = await prisma.user.findUnique({
+      where: {
+        nickname: nickname,
+      },
+      NOT: {
+        id: id,
+      },
+    });
+    if (sameNicknameUser) throw new Error('이미 존재하는 닉네임입니다.');
+
+    const updateUser = await prisma.user.update({
+      where: {
+        id: id,
+      },
+      data: {
+        name: name,
+        nickname: nickname,
+        password: hashedPassword,
+        about: about, //빈 값 허용
+        activity: activity, //빈 값 허용
+      },
+    });
+    console.log('유저의 정보를 업데이트했습니다');
+    return updateUser;
+  } catch (error) {
+    throw error;
+  }
+  /*
 	email - 고유값이어야 함
 	password = not null
 	about
@@ -107,23 +123,24 @@ const changeInformation = async (userData) => {
 };
 
 const removeUser = async (id) => {
-	try {
-		const deleteUser = await prisma.user.delete({
-			where: {
-				id: id,
-			},
-		});
-		console.log('탈퇴 완료');
-		return deleteUser;
-	} catch (error) {
-		throw error;
-	}
+  try {
+    const deleteUser = await prisma.user.delete({
+      where: {
+        id: id,
+      },
+    });
+    console.log('탈퇴 완료');
+    return deleteUser;
+  } catch (error) {
+    throw error;
+  }
 };
 
 module.exports = {
-	createUser,
-	getUserByEmail,
-	changePassword,
-	changeInformation,
-	removeUser,
+  createUser,
+  getUserByEmail,
+  changePassword,
+  changeInformation,
+  removeUser,
+  updateTokenByEmail,
 };
