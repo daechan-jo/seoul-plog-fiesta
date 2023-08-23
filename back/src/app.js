80b7112376d64158cd510229c49913497f3cd5ce
JoDaeChan <daechan476@gmail.com>
2023-08-23 14:13:37 +0900

    :rocket: Feat: 우리들의 일그러진 실시간 채팅

--- back/src/app.js
+++ back/src/app.js
@@ -3,6 +3,8 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import errorMiddleware from './middlewares/errorMiddleware';
 import loggerMiddleware from './middlewares/loggerMiddleware';
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 import authRoutes from './routers/authRouter';
 import userRoutes from './routers/userRouter';
 import groupRoutes from './routers/groupRouter';
@@ -10,21 +12,24 @@ import uploadRouter from './routers/uploadRouter';
 import loadRouter from './routers/loadRouter';
 import commentRouter from './routers/commentRouter';
 import { local, jwt } from './config';
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+import http from 'http';
+import socketIo from 'socket.io';
 const passport = require('passport');
-const app = express();
+const socketIoJwt = require('socketio-jwt');
 
-app.use('/upload', express.static('public'));
-passport.use('local', local);
-passport.use('jwt', jwt);
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, { path: '/chat' });
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(loggerMiddleware);
 app.use(passport.initialize());
+passport.use('local', local);
+passport.use('jwt', jwt);
 
 app.use(authRoutes);
 app.use(userRoutes);
@@ -34,6 +39,78 @@ app.use(loadRouter);
 app.use(commentRouter);
 
 app.use(errorMiddleware);
+
+/** @description 실시간 채팅 */
+io.use(
+	socketIoJwt.authorize({
+		secret: process.env.JWT_SECRET_KEY,
+		handshake: true,
+		auth_header_required: true,
+	}),
+);
+
+io.on('connection', (socket) => {
+	// 인증된 사용자 정보는 소켓.decoded_token에서 확인 가능
+	const loggedInUserId = socket.decoded_token.id;
+
+	socket.on('joinRoom', async (otherUserId) => {
+		// 나와 상대방 ID를 기반으로 고유한 방 ID를 생성
+		const roomId = `chat_${Math.min(loggedInUserId, otherUserId)}_${Math.max(
+			loggedInUserId,
+			otherUserId,
+		)}`;
+
+		// 방 들어가기
+		socket.join(roomId);
+
+		// roomId에 해당하는 기존 채팅 메시지가 있는 경우 검색하고 내보냄
+		const messages = await prisma.chatMessage.findMany({
+			where: { roomId },
+		});
+
+		socket.emit('messages', messages);
+	});
+
+	socket.on('sendMessage', async (otherUserId, message) => {
+		// 마찬가지로 나와 상대방 ID를 기반으로 룸 ID를 만든 다음 보낼 메시지를 데이터베이스에 저장하고 상대방에게 보낸다.
+		const roomId = `chat_${Math.min(loggedInUserId, otherUserId)}_${Math.max(
+			loggedInUserId,
+			otherUserId,
+		)}`;
+
+		await prisma.chatMessage.create({
+			data: {
+				roomId,
+				message,
+				senderId: loggedInUserId,
+			},
+		});
+		// 상대방에게 브로드캐스팅
+		io.to(roomId).emit('message', { senderId: loggedInUserId, message });
+	});
+
+	socket.on('leaveRoom', async (otherUserId) => {
+		const roomId = `chat_${Math.min(loggedInUserId, otherUserId)}_${Math.max(
+			loggedInUserId,
+			otherUserId,
+		)}`;
+
+		// 사용자가 방을 나가면, 해당 방과 속해있는 메시지를 데이터베이스에서 삭제한다
+		await prisma.chatMessage.deleteMany({
+			where: { roomId },
+		});
+		await prisma.chatRoom.delete({
+			where: { id: roomId },
+		});
+		socket.leave(roomId);
+	});
+
+	socket.on('disconnect', () => {
+		console.log(`User ${loggedInUserId} disconnected`);
+	});
+});
+
+app.io = io;
 
 /** @description 프로세스 종료 후 프리즈마 연결해제 */
 process.on('SIGINT', async () => {
