324fc6a7479dd8fc1fab2f098c519cf3e48842a8
a-honey <jungahyeon0512@khu.ac.kr>
2023-08-24 03:57:56 +0900

    :sparkles: Feat: 비밀번호 변경 요청 api 추가

--- front/src/components/user/PasswordChange.js
+++ front/src/components/user/PasswordChange.js
@@ -1,21 +1,30 @@
 import styles from './user.module.scss';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { AiOutlineArrowLeft } from "react-icons/ai"
-// import App from "../../App.js"
+import { AiOutlineArrowLeft } from 'react-icons/ai';
+import * as Api from '../../api';
+
 const PasswordChange = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-
-  const goBack = () => {
-    navigate(-1);
+    try {
+      await Api.post(`/auth/setpassword`, { email, name });
+      alert('비밀번호 변경 요청 성공! 이메일을 확인해주세요');
+      navigate('/login');
+    } catch (err) {
+      alert('비밀번호 변경 요청 실패');
+      console.log(err);
+    }
   };
+
   return (
-    <form className={styles.container}>
-      <AiOutlineArrowLeft className={styles.arrowLeft} onClick={goBack} />
+    <form className={styles.container} onSubmit={handleSubmit}>
+      <AiOutlineArrowLeft className={styles.arrowLeft} />
       <label>이름</label>
       <input
         type="text"
