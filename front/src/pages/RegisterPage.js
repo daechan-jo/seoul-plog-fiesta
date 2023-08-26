09d7cab2d422061e1eed83120ddcee5794cf7fa2
a-honey <jungahyeon0512@khu.ac.kr>
2023-08-26 09:27:37 +0900

    :package: Chore: 로그인 hook user 없는 경우 조건 추가

--- front/src/pages/RegisterPage.js
+++ front/src/pages/RegisterPage.js
@@ -8,9 +8,10 @@ const RegistePage = () => {
   const navigator = useNavigate();
   const user = useSelector((state) => state.user);
   const token = sessionStorage.getItem('userToken');
+
   // user의 상태가 존재하면 홈 페이지로 이동시킴
   useEffect(() => {
-    if (token) {
+    if (token && !user.email === '') {
       navigator('/');
     }
   });
