98f59c42c019316be0626c3b036dbdea624c38e3
a-honey <jungahyeon0512@khu.ac.kr>
2023-08-27 19:41:53 +0900

    :sparkles: Feat: 친구 삭제 api 연결

--- front/src/components/my/MyUsers.js
+++ front/src/components/my/MyUsers.js
@@ -6,15 +6,16 @@ import * as Api from '../../api';
 const MyUsers = () => {
   const [datas, setDatas] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
 
   useEffect(() => {
     const getData = async () => {
       try {
         setIsFetching(true);
-        const res = await Api.get(`/friend`);
+        const res = await Api.get(`/friends`);
         console.log('res', res);
-        if (res.data) {
-          setDatas(res.data);
+        if (res.data.friendsList) {
+          setDatas(res.data.friendsList.map((data) => data.userB));
         } else {
           setDatas([]);
         }
@@ -32,7 +33,14 @@ const MyUsers = () => {
     <div className="gContainer">
       <div className="titleContainer">
         <h1>나의 친구들</h1>
-        <button className="gBtn">친구 관리</button>
+        <button
+          className="gBtn"
+          onClick={() => {
+            setIsEditing(!isEditing);
+          }}
+        >
+          친구 관리
+        </button>
       </div>
       <div className={styles.shortBox}>
         {isFetching ? (
@@ -41,7 +49,11 @@ const MyUsers = () => {
           <div>데이터가 없습니다</div>
         ) : (
           datas.map((data) => (
-            <MyUser key={`my_group_list_${data.id}`} data={data} />
+            <MyUser
+              isEditing={isEditing}
+              key={`my_group_list_${data.id}`}
+              data={data}
+            />
           ))
         )}
       </div>
@@ -51,8 +63,22 @@ const MyUsers = () => {
 
 export default MyUsers;
 
-const MyUser = ({ data }) => {
+const MyUser = ({ data, isEditing }) => {
   const navigator = useNavigate();
+
+  const handleDelete = async () => {
+    const confirmDelete = window.confirm('정말로 삭제하시겠습니까?');
+
+    if (confirmDelete) {
+      try {
+        await Api.get(`/user/drop/${data.id}`);
+      } catch (err) {
+        console.log('친구 삭제 실패.', err);
+      }
+    } else {
+      console.log('친구 삭제가 취소되었습니다.');
+    }
+  };
   return (
     <div
       className={styles.myGroup}
@@ -63,8 +89,12 @@ const MyUser = ({ data }) => {
       <div className={styles.imgContainer}>
         <img src={data.imgUrl} alt="이미지" />
       </div>
-      <div>{data.name}</div>
-      <div>{data.score}</div>
+      <div>{data.nickname}</div>
+      {isEditing && (
+        <button className={styles.delete} onClick={handleDelete}>
+          X
+        </button>
+      )}
     </div>
   );
 };
