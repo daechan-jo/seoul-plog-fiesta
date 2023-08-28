d20bd63e41472f348d471d65a30ef7d917b3a211
a-honey <jungahyeon0512@khu.ac.kr>
2023-08-28 15:31:59 +0900

    :sparkles: Feat: 나의 인증글 조회 및 인증 모달 연결

--- front/src/components/home/PostList.js
+++ front/src/components/home/PostList.js
@@ -1,7 +1,11 @@
+import styles from './index.module.scss';
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import * as Api from '../../api';
 import { useSelector } from 'react-redux';
+import { handlePagenation } from '../../utils/pagenation';
+import Pagination from '../common/Pagenation';
+import PloggingShow from '../common/PlogginShow';
+import { handleCreatedDate } from '../../utils/handleCreatedDate';
 
 const PostList = ({ view }) => {
   const [isFetching, setIsFetching] = useState(false);
@@ -9,11 +13,20 @@ const PostList = ({ view }) => {
 
   const user = useSelector((state) => state.user);
 
+  const itemsPerPage = 20;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const paginatedData = handlePagenation(datas, currentPage, itemsPerPage);
+
+  const handlePage = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   useEffect(() => {
     const getData = async () => {
       try {
         setIsFetching(true);
-        const res = await Api.get(`/plo/board/${user.loginId}`);
+        const res = await Api.get(`/user/cert/list`);
         console.log(res);
         setDatas(res.data);
       } catch (err) {
@@ -31,33 +44,55 @@ const PostList = ({ view }) => {
       <div className="titleContainer">
         <h1>나의 인증글보기</h1>
       </div>
-      <div className="contentListContainer">
+      <div className={styles.postList}>
         {isFetching ? (
           <div>로딩중</div>
         ) : datas?.length === 0 ? (
           <div>데이터가 없습니다.</div>
         ) : (
-          datas.map((data) => <Item data={data} key={data.id} view={view} />)
+          paginatedData.map((data) => (
+            <Item data={data} key={data.id} view={view} />
+          ))
         )}
       </div>
-      <div>페이지네이션자리</div>
+      <div>
+        <Pagination
+          totalPages={Math.ceil(datas.length / itemsPerPage)}
+          currentPage={currentPage}
+          handlePage={handlePage}
+        />
+      </div>
     </div>
   );
 };
 
 export default PostList;
 
-const Item = ({ data, view }) => {
-  const navigator = useNavigate();
+/*
+[{"id":13,"writerId":5,"title":"ㅇㄹㅁㅇ","region":"gangbuk","location":"124","distance":"424","trashAmount":"424","averagePace":"424","description":"424","startTime":"15","endTime":"456","createdAt":"2023-08-27T18:17:03.480Z","isGroupPost":false,"groupName":null},
+*/
+const Item = ({ data }) => {
+  const [isPlogginShowOpen, setIsPlogginShowOpen] = useState(false);
 
   return (
-    <div
-      onClick={() => {
-        navigator(`/posts/${data.id}`);
-      }}
-    >
-      <div>나의 인증1</div>
-      <div>장소 데이터있으면 여러개가 뜰것,,임,,</div>
-    </div>
+    <>
+      {isPlogginShowOpen && (
+        <PloggingShow
+          id={data.id}
+          setIsPlogginShowOpen={setIsPlogginShowOpen}
+        />
+      )}
+      <div
+        className={styles.postItem}
+        onClick={() => {
+          setIsPlogginShowOpen(true);
+        }}
+      >
+        <div>{data.id}</div>
+        <div>|</div>
+        <div>{data.title}</div>
+        <div>{handleCreatedDate(data.createdAt)}</div>
+      </div>
+    </>
   );
 };
