0a1edf653051e3efcb4d4d0e6a7b8b216c3dae32
a-honey <jungahyeon0512@khu.ac.kr>
2023-08-28 16:47:09 +0900

    :lipstick: Design: 랭킹페이지 UI

--- front/src/components/ranking/All.js
+++ front/src/components/ranking/All.js
@@ -2,10 +2,21 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import * as Api from '../../api';
 import styles from './index.module.scss';
+import Pagination from '../common/Pagenation';
+import { handlePagenation } from '../../utils/pagenation';
 
 const All = ({ view }) => {
   const [isFetching, setIsFetching] = useState(false);
   const [datas, setDatas] = useState([]);
+
+  const itemsPerPage = 20;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const paginatedData = handlePagenation(datas, currentPage, itemsPerPage);
+
+  const handlePage = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   useEffect(() => {
     const getData = async () => {
@@ -34,8 +45,18 @@ const All = ({ view }) => {
         ) : datas?.length === 0 ? (
           <div>데이터가 없습니다.</div>
         ) : (
-          datas.map((data) => <Item data={data} key={data.id} view={view} />)
+          paginatedData.map((data) => (
+            <Item data={data} key={data.id} view={view} />
+          ))
         )}
+      </div>
+      <div>
+        {' '}
+        <Pagination
+          totalPages={Math.ceil(datas.length / itemsPerPage)}
+          currentPage={currentPage}
+          handlePage={handlePage}
+        />
       </div>
     </div>
   );
@@ -59,7 +80,11 @@ const Item = ({ data, view }) => {
 
   return (
     <div
-      className={styles.rankingItem}
+      className={
+        data.rank <= 3
+          ? `${styles.rankingItem} ${styles.rankingTop3}`
+          : styles.rankingItem
+      }
       onClick={() => {
         navigator(`/users/${data.id}`);
       }}
