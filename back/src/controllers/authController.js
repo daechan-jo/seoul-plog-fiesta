84752de3af32d662b770ab58b26ad77d047b040d
[AI8/1반] 정현수 <conjugate0519@gmail.com>
2023-08-29 12:47:40 +0900

    ⚡️ Fix: 리다이렉트 주소 수정

--- back/src/controllers/authController.js
+++ back/src/controllers/authController.js
@@ -102,7 +102,7 @@ const checkEmail = async (req, res, next) => {
     const email = updatedUser.email;
     // 유저 이메일 인증 처리후 리다이렉트 -> 프론트로
     res.redirect(
-      'http://localhost:3000/auth/changepassword?email=' +
+      'http://localhost:3000/changepassword?email=' +
         email +
         '&token=' +
         passwordToken,
