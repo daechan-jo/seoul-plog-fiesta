e9ba0d3b4bb9b1cc9ba2e1a74391fa2e723d4361
a-honey <jungahyeon0512@khu.ac.kr>
2023-09-01 00:13:18 +0900

    :rocket: Chore: vM 서버 연결

--- front/src/api/instance.js
+++ front/src/api/instance.js
@@ -1,24 +1,24 @@
 import axios from 'axios';
 
-// const baseURL = 'http://34.64.122.168:3001';
-const baseURL = 'http://localhost:3001';
+const baseURL = 'http://34.64.122.168:3001';
+//const baseURL = 'http://localhost:3001';
 
 const userToken = () => localStorage.getItem('userToken');
 
 export const instance = axios.create({
-	baseURL,
-	timeout: 5000,
-	headers: {
-		'Content-Type': 'application/json',
-		Authorization: `Bearer ${userToken()}`,
-	},
+  baseURL,
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${userToken()}`,
+  },
 });
 
 export const formDataInstance = axios.create({
-	baseURL,
-	timeout: 3000,
-	headers: {
-		'Content-Type': 'multipart/form-data',
-		Authorization: `Bearer ${userToken()}`,
-	},
+  baseURL,
+  timeout: 3000,
+  headers: {
+    'Content-Type': 'multipart/form-data',
+    Authorization: `Bearer ${userToken()}`,
+  },
 });
