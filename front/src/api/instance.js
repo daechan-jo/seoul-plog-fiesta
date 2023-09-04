a0649dfef5efa4c948ad8b44a64bbf6304af2f6e
a-honey <jungahyeon0512@khu.ac.kr>
2023-09-04 16:59:07 +0900

    packages: Chore: axios intercepts 구현 성공

--- front/src/api/instance.js
+++ front/src/api/instance.js
@@ -2,22 +2,20 @@ import axios from 'axios';
 
 const baseURL = 'http://34.64.122.168:3001';
 
-const userToken = () => localStorage.getItem('userToken');
-
 export const instance = axios.create({
   baseURL,
   timeout: 5000,
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${userToken()}`,
-  },
 });
 
 instance.interceptors.request.use(
   (config) => {
-    console.log('인터셉트해버리기');
+    const userToken = localStorage.getItem('userToken');
+
     config.headers['Content-Type'] = 'application/json';
-    config.headers['Authorization'] = `Bearer ${userToken()}`;
+    config.headers['Authorization'] = `Bearer ${userToken}`;
+
+    // refreshToken 요청
+    
     return config;
   }, (error)=>{
     console.log('에러를 인터셉트해버리기') 
@@ -28,20 +26,17 @@ instance.interceptors.request.use(
 export const formDataInstance = axios.create({
   baseURL,
   timeout: 3000,
-  headers: {
-    'Content-Type': 'multipart/form-data',
-    Authorization: `Bearer ${userToken()}`,
-  },
 });
 
-const formDataInstanceInterceptor = formDataInstance.interceptors.request.use(
+formDataInstance.interceptors.request.use(
   (config) => {
-    console.log('폼 인터셉트해버리기')
+    const userToken = localStorage.getItem('userToken');
+
+    config.headers['Content-Type'] = 'application/json';
+    config.headers['Authorization'] = `Bearer ${userToken}`;
     return config;
   }, (error)=>{
     console.log('폼 에러를 인터셉트해버리기') 
     return Promise.reject(error);
   }
 );
-
-formDataInstance.interceptors.request.eject(formDataInstanceInterceptor);
