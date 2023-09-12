9951a95c7fd34abeebbf603df4c21c96cbb57166
JoDaeChan <daechan476@gmail.com>
2023-09-13 01:19:53 +0900

    :hammer: Refactor: chatService mdularization

---
+++ back/src/services/chatService.js
@@ -0,0 +1,113 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+const createRoomId = async (loggedInUserId, otherUserId) => {
+  try {
+    return `chat_${Math.min(loggedInUserId, otherUserId)}_${Math.max(
+      loggedInUserId,
+      otherUserId,
+    )}`;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getUserById = async (loggedInUserId) => {
+  try {
+    return await prisma.user.findUnique({
+      where: { id: loggedInUserId },
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getUnreadMessages = async (userId) => {
+  try {
+    return prisma.$queryRaw`
+      SELECT * FROM ChatMessage
+			WHERE (
+						roomId LIKE CONCAT('chat_', ${userId}, '_%') OR
+						roomId LIKE CONCAT('chat_%_', ${userId})
+				)
+        AND senderId != ${userId}
+        AND isRead = false
+      ORDER BY createdAt DESC, senderId ASC
+    `;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getChatRoom = async (roomId) => {
+  try {
+    return await prisma.chatRoom.findUnique({
+      where: { id: roomId },
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getMessagesByRoomId = async (roomId) => {
+  try {
+    return await prisma.chatMessage.findMany({
+      where: { roomId },
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const isReadUpdate = async (messageId) => {
+  try {
+    return await prisma.chatMessage.update({
+      where: { id: messageId },
+      data: { isRead: true },
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const createChatRoom = async (roomId) => {
+  try {
+    return await prisma.chatRoom.create({
+      data: { id: roomId },
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const deleteChatMessages = async (roomId) => {
+  try {
+    return await prisma.chatMessage.deleteMany({
+      where: { roomId },
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const deleteChatRoom = async (roomId) => {
+  try {
+    return await prisma.chatRoom.delete({
+      where: { id: roomId },
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = {
+  getUserById,
+  getUnreadMessages,
+  getChatRoom,
+  getMessagesByRoomId,
+  isReadUpdate,
+  createChatRoom,
+  createRoomId,
+  deleteChatMessages,
+  deleteChatRoom,
+};
--- back/src/socket.js
+++ back/src/socket.js
@@ -1,9 +1,11 @@
+import chatService from './services/chatService';
 const socketIo = require('socket.io');
 const socketIoJwt = require('socketio-jwt');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-function initializeSocketServer(server) {
+async function initializeSocketServer(server) {
+  const connectedUsers = {};
   const io = socketIo(server, {
     path: '/chat',
     cors: {
@@ -20,16 +22,14 @@ function initializeSocketServer(server) {
     }),
   );
 
-  const connectedUsers = {};
-
   io.on('connection', async (socket) => {
     socket.onAny((e) => {
       console.log(`Socket Event: ${e}`);
     });
+
     const loggedInUserId = socket.decoded_token.id;
-    const user = await prisma.user.findUnique({
-      where: { id: loggedInUserId },
-    });
+
+    const user = await chatService.getUserById(loggedInUserId);
     connectedUsers[loggedInUserId] = {
       socketId: socket.id,
       roomId: null,
@@ -39,16 +39,7 @@ function initializeSocketServer(server) {
 
     // 안읽은 메시지 불러오기
     socket.on('initialize', async (userId) => {
-      const messages = await prisma.$queryRaw`
-      SELECT * FROM ChatMessage
-			WHERE (
-						roomId LIKE CONCAT('chat_', ${userId}, '_%') OR
-						roomId LIKE CONCAT('chat_%_', ${userId})
-				)
-        AND senderId != ${userId}
-        AND isRead = false
-      ORDER BY createdAt DESC, senderId ASC
-    `;
+      const messages = await chatService.getUnreadMessages(userId);
       socket.emit('messages', messages);
       console.log(messages, '새로운 메시지');
     });
@@ -56,40 +47,31 @@ function initializeSocketServer(server) {
     /** @description 사용자와 상대방의 아이디 기반 고유 roomId 생성
      * 해당 아이디로 이미 생성된 방이 있다면 채팅 내역을 불러오고 읽음처리*/
     socket.on('joinRoom', async (otherUserId) => {
-      const roomId = `chat_${Math.min(loggedInUserId, otherUserId)}_${Math.max(
+      const roomId = await chatService.createRoomId(
         loggedInUserId,
         otherUserId,
-      )}`;
+      );
 
-      const existingRoom = await prisma.chatRoom.findUnique({
-        where: { id: roomId },
-      });
+      const existingRoom = await chatService.getChatRoom(roomId);
 
       if (existingRoom) {
         connectedUsers[loggedInUserId].roomId = roomId;
         console.log(connectedUsers[loggedInUserId].roomId, '현재 접속항방');
         socket.join(roomId);
         console.log(`${user.nickname}님이 [${roomId}] 방에 접속했습니다.`);
-        const messages = await prisma.chatMessage.findMany({
-          where: { roomId },
-        });
+        const messages = await chatService.getMessagesByRoomId(roomId);
 
         for (let message of messages) {
           if (message.senderId !== loggedInUserId) {
             if (!message.isRead) {
-              await prisma.chatMessage.update({
-                where: { id: message.id },
-                data: { isRead: true },
-              });
+              await chatService.isReadUpdate(message.id);
             }
           }
         }
         socket.emit('messages', messages);
         console.log(`${user.nickname}님의 채팅 내역 불러오기 완료`);
       } else {
-        const newRoom = await prisma.chatRoom.create({
-          data: { id: roomId },
-        });
+        const newRoom = await chatService.createChatRoom(roomId);
         console.log(`[${newRoom}] 방 생성 완료`);
         connectedUsers[loggedInUserId].roomId = roomId;
         socket.join(newRoom.id);
@@ -99,18 +81,14 @@ function initializeSocketServer(server) {
     /** @description 메시지 보내기
      * 메시지를 데이터베이스에 저장 후 상대방이 해당 방에 접속중이라면 전송 후 읽음처리 */
     socket.on('sendMessage', async (otherUserId, message) => {
-      const roomId = `chat_${Math.min(loggedInUserId, otherUserId)}_${Math.max(
+      const roomId = await chatService.createRoomId(
         loggedInUserId,
         otherUserId,
-      )}`;
+      );
 
-      let room = await prisma.chatRoom.findUnique({
-        where: { id: roomId },
-      });
+      let room = await chatService.getChatRoom(roomId);
       if (!room) {
-        await prisma.chatRoom.create({
-          data: { id: roomId },
-        });
+        await chatService.createChatRoom(roomId);
       }
 
       const createdMessage = await prisma.chatMessage.create({
@@ -134,10 +112,7 @@ function initializeSocketServer(server) {
           });
           console.log(`[${otherUserId}] 에게 메시지 :: ${message}`);
 
-          await prisma.chatMessage.update({
-            where: { id: createdMessage.id },
-            data: { isRead: true },
-          });
+          await chatService.isReadUpdate(createdMessage.id);
         }
 
         if (connectedUsers[otherUserId].roomId === null) {
@@ -152,10 +127,10 @@ function initializeSocketServer(server) {
 
     /** @description 방 나가기 */
     socket.on('leaveRoom', async (otherUserId) => {
-      const roomId = `chat_${Math.min(loggedInUserId, otherUserId)}_${Math.max(
+      const roomId = await chatService.createRoomId(
         loggedInUserId,
         otherUserId,
-      )}`;
+      );
 
       console.log(`[${roomId}] 방에서 나가기`);
       connectedUsers[loggedInUserId].roomId = null;
@@ -170,20 +145,13 @@ function initializeSocketServer(server) {
         otherUserId,
       )}`;
 
-      await prisma.chatMessage.deleteMany({
-        where: { roomId },
-      });
-      await prisma.chatRoom.delete({
-        where: { id: roomId },
-      });
+      await chatService.deleteChatMessages(roomId);
+      await chatService.deleteChatRoom(roomId);
       console.log(`[${roomId}] 방 삭제 완료`);
     });
 
     socket.on('messageViewed', async (messageId) => {
-      await prisma.chatMessage.update({
-        where: { id: messageId },
-        data: { isRead: true },
-      });
+      await chatService.isReadUpdate(messageId);
     });
 
     socket.on('disconnect', () => {
