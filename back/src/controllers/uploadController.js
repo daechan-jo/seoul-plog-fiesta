999ff017a3a8d01ab4c30ad845018908e42821c0
JoDaeChan <daechan476@gmail.com>
2023-09-12 11:21:44 +0900

    :hammer: Refactor: user router
    잘못된 상태코드 수정
    res 객체에 명시적 return 추가

--- back/src/controllers/uploadController.js
+++ back/src/controllers/uploadController.js
@@ -4,168 +4,164 @@ const path = require('path');
 const fs = require('fs');
 
 const uploadProfileImage = async (req, res, next) => {
-	try {
-		const userId = req.user.id;
-		const imageUrl = path.join('images', req.file.filename);
-		const existingImage = await prisma.userProfileImage.findUnique({
-			where: { userId },
-		});
+  try {
+    const userId = req.user.id;
+    const imageUrl = path.join('images', req.file.filename);
+    const existingImage = await prisma.userProfileImage.findUnique({
+      where: { userId },
+    });
 
-		if (existingImage) {
-			const absoluteImagePath = path.join(
-				__dirname,
-				'../..',
-				'public',
-				existingImage.imageUrl,
-			);
-			fs.unlinkSync(absoluteImagePath);
+    if (existingImage) {
+      const absoluteImagePath = path.join(
+        __dirname,
+        '../..',
+        'public',
+        existingImage.imageUrl,
+      );
+      fs.unlinkSync(absoluteImagePath);
 
-			await prisma.userProfileImage.update({
-				where: { id: existingImage.id },
-				data: { imageUrl },
-			});
-		} else {
-			await prisma.userProfileImage.create({
-				data: {
-					imageUrl,
-					userId,
-				},
-			});
-		}
-		res.status(201).json(imageUrl);
-	} catch (error) {
-		console.error(error);
-		error.status = 500;
-		next(error);
-	}
+      await prisma.userProfileImage.update({
+        where: { id: existingImage.id },
+        data: { imageUrl },
+      });
+    } else {
+      await prisma.userProfileImage.create({
+        data: {
+          imageUrl,
+          userId,
+        },
+      });
+    }
+    return res.status(201).json(imageUrl);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 };
 
 const uploadPostImage = async (req, res, next) => {
-	try {
-		const postId = parseInt(req.params.postid);
-		const imageUrl = path.join('images', req.file.filename);
-		const post = await prisma.post.findUnique({
-			where: { id: postId },
-			include: { group: true },
-		});
+  try {
+    const postId = parseInt(req.params.postid);
+    const imageUrl = path.join('images', req.file.filename);
+    const post = await prisma.post.findUnique({
+      where: { id: postId },
+      include: { group: true },
+    });
 
-		if (!post) {
-			return res.status(404).json({ message: '게시글 없음' }); // Corrected line
-		}
+    if (!post) {
+      return res.status(404).json({ message: '게시글 없음' }); // Corrected line
+    }
 
-		const existingImage = await prisma.postImage.findFirst({
-			where: { postId: post.id },
-		});
+    const existingImage = await prisma.postImage.findFirst({
+      where: { postId: post.id },
+    });
 
-		if (existingImage) {
-			fs.unlinkSync(
-				path.join(__dirname, '../..', 'public', existingImage.imageUrl),
-			);
+    if (existingImage) {
+      fs.unlinkSync(
+        path.join(__dirname, '../..', 'public', existingImage.imageUrl),
+      );
 
-			await prisma.postImage.update({
-				where: { id: existingImage.id },
-				data: { imageUrl },
-			});
-		} else {
-			await prisma.postImage.create({
-				data: {
-					imageUrl,
-					post: { connect: { id: postId } },
-				},
-			});
-		}
-		res.status(201).json(imageUrl);
-	} catch (error) {
-		console.error(error);
-		error.status = 500;
-		next(error);
-	}
+      await prisma.postImage.update({
+        where: { id: existingImage.id },
+        data: { imageUrl },
+      });
+    } else {
+      await prisma.postImage.create({
+        data: {
+          imageUrl,
+          post: { connect: { id: postId } },
+        },
+      });
+    }
+    return res.status(201).json(imageUrl);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 };
 
 const uploadCertImage = async (req, res, next) => {
-	try {
-		const certPostId = parseInt(req.params.postid);
-		const imageUrl = path.join('images', req.file.filename);
-		const certPost = await prisma.certPost.findUnique({
-			where: { id: certPostId },
-		});
-		if (!certPost) {
-			return res.status(404).json({ message: '인증 게시글 없음' });
-		}
-		const existingImage = await prisma.certPostImage.findFirst({
-			where: { certPostId: certPost.id },
-		});
-		if (existingImage) {
-			fs.unlinkSync(
-				path.join(__dirname, '../..', 'public', existingImage.imageUrl),
-			);
-			await prisma.certPostImage.update({
-				where: { id: existingImage.id },
-				data: { imageUrl },
-			});
-		} else {
-			await prisma.certPostImage.create({
-				data: {
-					imageUrl,
-					certPost: { connect: { id: certPostId } },
-				},
-			});
-		}
-		res.status(201).json(imageUrl);
-	} catch (error) {
-		console.error(error);
-		error.status = 500;
-		next(error);
-	}
+  try {
+    const certPostId = parseInt(req.params.postid);
+    const imageUrl = path.join('images', req.file.filename);
+    const certPost = await prisma.certPost.findUnique({
+      where: { id: certPostId },
+    });
+    if (!certPost) {
+      return res.status(404).json({ message: '인증 게시글 없음' });
+    }
+    const existingImage = await prisma.certPostImage.findFirst({
+      where: { certPostId: certPost.id },
+    });
+    if (existingImage) {
+      fs.unlinkSync(
+        path.join(__dirname, '../..', 'public', existingImage.imageUrl),
+      );
+      await prisma.certPostImage.update({
+        where: { id: existingImage.id },
+        data: { imageUrl },
+      });
+    } else {
+      await prisma.certPostImage.create({
+        data: {
+          imageUrl,
+          certPost: { connect: { id: certPostId } },
+        },
+      });
+    }
+    return res.status(201).json(imageUrl);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 };
 
 const uploadGroupImage = async (req, res, next) => {
-	try {
-		const userId = req.user.id;
-		const groupId = parseInt(req.params.groupid);
-		const isGroupAdmin = await prisma.group.findFirst({
-			where: {
-				AND: [{ id: groupId }, { managerId: userId }],
-			},
-		});
-		if (!isGroupAdmin) {
-			return res.status(403).json({ message: '관리자 권한' });
-		}
-		const imageUrl = path.join('images', req.file.filename);
-		const existingImage = await prisma.groupImage.findFirst({
-			where: { groupId },
-		});
-		if (existingImage) {
-			const absoluteImagePath = path.join(
-				__dirname,
-				'../..',
-				'public',
-				existingImage.imageUrl,
-			);
-			fs.unlinkSync(absoluteImagePath);
-			await prisma.groupImage.update({
-				where: { id: existingImage.id },
-				data: { imageUrl },
-			});
-		} else {
-			await prisma.groupImage.create({
-				data: {
-					imageUrl,
-					groupId,
-				},
-			});
-		}
-		res.status(201).json(imageUrl);
-	} catch (error) {
-		console.error(error);
-		error.status = 500;
-		next(error);
-	}
+  try {
+    const userId = req.user.id;
+    const groupId = parseInt(req.params.groupid);
+    const isGroupAdmin = await prisma.group.findFirst({
+      where: {
+        AND: [{ id: groupId }, { managerId: userId }],
+      },
+    });
+    if (!isGroupAdmin) {
+      return res.status(403).json({ message: '관리자 권한' });
+    }
+    const imageUrl = path.join('images', req.file.filename);
+    const existingImage = await prisma.groupImage.findFirst({
+      where: { groupId },
+    });
+    if (existingImage) {
+      const absoluteImagePath = path.join(
+        __dirname,
+        '../..',
+        'public',
+        existingImage.imageUrl,
+      );
+      fs.unlinkSync(absoluteImagePath);
+      await prisma.groupImage.update({
+        where: { id: existingImage.id },
+        data: { imageUrl },
+      });
+    } else {
+      await prisma.groupImage.create({
+        data: {
+          imageUrl,
+          groupId,
+        },
+      });
+    }
+    return res.status(201).json(imageUrl);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 };
 
 module.exports = {
-	uploadProfileImage,
-	uploadPostImage,
-	uploadGroupImage,
-	uploadCertImage,
+  uploadProfileImage,
+  uploadPostImage,
+  uploadGroupImage,
+  uploadCertImage,
 };
--- back/src/controllers/userController.js
+++ back/src/controllers/userController.js
@@ -3,243 +3,238 @@ import ploService from '../services/ploService.js';
 
 /** @description 모든 유저  */
 const getAllUsers = async (req, res, next) => {
-	try {
-		const page = req.query.page !== undefined ? parseInt(req.query.page) : null;
-		const limit = req.query.limit !== undefined ? parseInt(req.query.limit) : null;
+  try {
+    const page = req.query.page !== undefined ? parseInt(req.query.page) : null;
+    const limit =
+      req.query.limit !== undefined ? parseInt(req.query.limit) : null;
 
-		const users = await userService.getAllUsers(page, limit);
+    const users = await userService.getAllUsers(page, limit);
 
-		if (users == 0) {
-			return res.status(404).json({ message: '유저 없음' });
-		}
+    if (users.length === 0) {
+      return res.status(404).json({ message: '유저 없음' });
+    }
 
-		res.status(200).json({ message: '모든 유저', users });
-	} catch (error) {
-		console.error(error);
-		error.status = 500;
-		next(error);
-	}
+    return res.status(200).json({ message: '모든 유저', users });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 };
-
 
 /** @description 유저 검색 */
 const searchUsers = async (req, res, next) => {
-	const nickname = req.params.name;
-	try {
-		const searchNickname = await userService.searchUsers(nickname);
+  const nickname = req.params.name;
+  try {
+    const searchNickname = await userService.searchUsers(nickname);
 
-		if (!searchNickname) {
-			return res.status(404).json({ message: '검색 결과 없음' });
-		}
+    if (!searchNickname) {
+      return res.status(404).json({ message: '검색 결과 없음' });
+    }
 
-		res.status(200).json({ message: '유저 검색 결과', searchNickname });
-	} catch (error) {
-		console.error(error);
-		error.status = 500;
-		next(error);
-	}
+    return res.status(200).json({ message: '유저 검색 결과', searchNickname });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 };
 
 /** @description 유저 찬기(id) */
 const searchUserId = async (req, res, next) => {
-	try {
-		const userId = parseInt(req.params.id);
-		const searchId = await userService.searchUserId(userId);
+  try {
+    const userId = parseInt(req.params.id);
+    const searchId = await userService.searchUserId(userId);
 
-		if (!searchId) {
-			return res.status(404).json({ message: '검색 결과 없음' });
-		}
+    if (!searchId) {
+      return res.status(404).json({ message: '검색 결과 없음' });
+    }
 
-		res.status(200).json({ message: '유저 검색 결과(id)', searchId });
-	} catch (error) {
-		console.error(error);
-		error.status = 500;
-		next(error);
-	}
+    return res.status(200).json({ message: '유저 검색 결과(id)', searchId });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 };
 
 /** @description 랜덤 유저 */
 const getRandomUsers = async (req, res, next) => {
-	try {
-		const randomUsers = await userService.getRandomUsers();
-		res.status(200).json({ message: '랜덤 유저', randomUsers });
-	} catch (error) {
-		console.error(error);
-		error.status = 500;
-		next(error);
-	}
+  try {
+    const randomUsers = await userService.getRandomUsers();
+    return res.status(200).json({ message: '랜덤 유저', randomUsers });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 };
 
 /** @description 현재 사용자 */
 const currentUser = async (req, res, next) => {
-	try {
-		const userId = req.user.id;
-		const currentUserInfo = await userService.getUserInfo(userId);
-		res.status(200).json({ message: '나의 상세 정보', currentUserInfo });
-	} catch (error) {
-		console.error(error);
-		error.status = 500;
-		next(error);
-	}
+  try {
+    const userId = req.user.id;
+    const currentUserInfo = await userService.getUserInfo(userId);
+    return res.status(200).json({ message: '나의 상세 정보', currentUserInfo });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 };
 
 /** @description 친구 요청 */
 const friendRequest = async (req, res, next) => {
-	try {
-		const userId = req.user.id;
-		const requestId = parseInt(req.params.id);
+  try {
+    const userId = req.user.id;
+    const requestId = parseInt(req.params.id);
 
-		if (userId === requestId) {
-			return res
-				.status(400)
-				.json({ message: '나 자신과는 친구가 될 수 없어!' });
-		}
-		const weAreFriends = await userService.weAreFriends(userId, requestId);
-		if (weAreFriends) {
-			return res.status(400).json({ message: '이미 요청 했거나 우린 친구!' });
-		}
-		const existingFriendRequest = await userService.weAreFriends(
-			requestId,
-			userId,
-		);
-		if (existingFriendRequest) {
-			return res.status(400).json({ message: '이미 요청 했거나 우린 친구!' });
-		}
-		const request = await userService.createFriendship(userId, requestId);
-		res.status(200).json({ message: '친구 요청 완료', request });
-	} catch (error) {
-		console.error(error);
-		error.status = 500;
-		next(error);
-	}
+    if (userId === requestId) {
+      return res
+        .status(400)
+        .json({ message: '나 자신과는 친구가 될 수 없어!' });
+    }
+    const weAreFriends = await userService.weAreFriends(userId, requestId);
+    if (weAreFriends) {
+      return res.status(400).json({ message: '이미 요청 했거나 우린 친구!' });
+    }
+    const existingFriendRequest = await userService.weAreFriends(
+      requestId,
+      userId,
+    );
+    if (existingFriendRequest) {
+      return res.status(400).json({ message: '이미 요청 했거나 우린 친구!' });
+    }
+    const request = await userService.createFriendship(userId, requestId);
+    return res.status(200).json({ message: '친구 요청 완료', request });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 };
 
 /** @description 친구 요청 목록 */
 const friendRequestList = async (req, res, next) => {
-	try {
-		const userId = req.user.id;
-		const friendRequest = await userService.friendRequestList(userId);
-		res.status(200).json({ message: '친구 요청 리스트', friendRequest });
-	} catch (error) {
-		console.error(error);
-		error.status = 500;
-		next(error);
-	}
+  try {
+    const userId = req.user.id;
+    const friendRequest = await userService.friendRequestList(userId);
+    return res.status(200).json({ message: '친구 요청 리스트', friendRequest });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 };
 
 /** @description 친구 수락 */
 const acceptFriend = async (req, res, next) => {
-	try {
-		const userId = req.user.id;
-		const requestId = parseInt(req.params.id);
-		const acceptFriend = await userService.acceptFriend(userId, requestId);
-		res.status(200).json({ message: '친구 수락', acceptFriend });
-	} catch (error) {
-		console.error(error);
-		error.status = 500;
-		next(error);
-	}
+  try {
+    const userId = req.user.id;
+    const requestId = parseInt(req.params.id);
+    const acceptFriend = await userService.acceptFriend(userId, requestId);
+    return res.status(200).json({ message: '친구 수락', acceptFriend });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 };
 
 /** @description 친구 거절 */
 const rejectFriend = async (req, res, next) => {
-	try {
-		const userId = req.user.id;
-		const requestId = parseInt(req.params.id);
-		const rejectFriend = await userService.rejectFriend(userId, requestId);
-		res.status(200).json({ message: '친구 거절', rejectFriend });
-	} catch (error) {
-		console.error(error);
-		error.status = 500;
-		next(error);
-	}
+  try {
+    const userId = req.user.id;
+    const requestId = parseInt(req.params.id);
+    const rejectFriend = await userService.rejectFriend(userId, requestId);
+    return res.status(200).json({ message: '친구 거절', rejectFriend });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 };
 
 /** @description 친구 목록 */
 const getMyFriends = async (req, res, next) => {
-	try {
-		const page = req.query.page !== undefined ? parseInt(req.query.page) : null;
-		const limit = req.query.limit !== undefined ? parseInt(req.query.limit) : null;
-		const userId = req.user.id;
-		const friendsList = await userService.getMyFriends(userId, page, limit);
-		res.status(200).json({ message: '친구 목록', friendsList });
-	} catch (error) {
-		console.error(error);
-		error.status = 500;
-		next(error);
-	}
+  try {
+    const page = req.query.page !== undefined ? parseInt(req.query.page) : null;
+    const limit =
+      req.query.limit !== undefined ? parseInt(req.query.limit) : null;
+    const userId = req.user.id;
+    const friendsList = await userService.getMyFriends(userId, page, limit);
+    return res.status(200).json({ message: '친구 목록', friendsList });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 };
 
 /** @description 친구 삭제 */
 const deleteFriend = async (req, res, next) => {
-	try {
-		const userId = req.user.id;
-		const friendId = parseInt(req.params.id);
-		const deleteFriend = await userService.deleteFriend(userId, friendId);
-		res.status(200).json({ message: '친구 삭제', deleteFriend });
-	} catch (error) {
-		console.error(error);
-		error.status = 500;
-		next(error);
-	}
+  try {
+    const userId = req.user.id;
+    const friendId = parseInt(req.params.id);
+    const deleteFriend = await userService.deleteFriend(userId, friendId);
+    return res.status(204).json({ message: '친구 삭제', deleteFriend });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 };
 
 /** @description 나의 인증 */
 const myScoreNRank = async (req, res, next) => {
-	try {
-		const userId = req.user.id;
-		const myCertPostCount = await userService.myScoreNRank(userId);
-		const myScore = myCertPostCount * 353;
-		const myRank = await ploService.getUserRank(userId)
-		res.status(200).json({ message: '나의 점수 / 랭킹', data :{myScore, myRank} });
-	} catch (error) {
-		console.error(error);
-		error.status = 500;
-		next(error);
-	}
+  try {
+    const userId = req.user.id;
+    const myCertPostCount = await userService.myScoreNRank(userId);
+    const myScore = myCertPostCount * 353;
+    const myRank = await ploService.getUserRank(userId);
+    return res
+      .status(200)
+      .json({ message: '나의 점수 / 랭킹', data: { myScore, myRank } });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 };
-
 
 /** @description 친구 최신 게시물 */
 const friendsRecentPost = async (req, res, next) => {
-	try {
-		const userId = req.user.id;
-		const friendsRecentPost = await userService.friendsRecentPost(userId);
-		res.status(200).json({ message: '친구 최신 게시물', friendsRecentPost });
-	} catch (error) {
-		console.error(error);
-		error.status = 500;
-		next(error);
-	}
+  try {
+    const userId = req.user.id;
+    const friendsRecentPost = await userService.friendsRecentPost(userId);
+    return res
+      .status(200)
+      .json({ message: '친구 최신 게시물', friendsRecentPost });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 };
 
 const getCertPostsByUserId = async (req, res, next) => {
-	try {
-		const page = req.query.page !== undefined ? parseInt(req.query.page) : null;
-		const limit = req.query.limit !== undefined ? parseInt(req.query.limit) : null;
-		const userId = req.user.id;
-		const certPosts = await userService.getCertPostsByUserId(userId, page, limit);
-		res.status(200).json(certPosts);
-	} catch (error) {
-		console.error(error);
-		error.status = 500;
-		next(error);
-	}
+  try {
+    const page = req.query.page !== undefined ? parseInt(req.query.page) : null;
+    const limit =
+      req.query.limit !== undefined ? parseInt(req.query.limit) : null;
+    const userId = req.user.id;
+    const certPosts = await userService.getCertPostsByUserId(
+      userId,
+      page,
+      limit,
+    );
+    return res.status(200).json(certPosts);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 };
 
 module.exports = {
-	getAllUsers,
-	searchUsers,
-	searchUserId,
-	getRandomUsers,
-	currentUser,
-	friendRequest,
-	friendRequestList,
-	acceptFriend,
-	rejectFriend,
-	getMyFriends,
-	deleteFriend,
-	myScoreNRank,
-	friendsRecentPost,
-	getCertPostsByUserId,
+  getAllUsers,
+  searchUsers,
+  searchUserId,
+  getRandomUsers,
+  currentUser,
+  friendRequest,
+  friendRequestList,
+  acceptFriend,
+  rejectFriend,
+  getMyFriends,
+  deleteFriend,
+  myScoreNRank,
+  friendsRecentPost,
+  getCertPostsByUserId,
 };
